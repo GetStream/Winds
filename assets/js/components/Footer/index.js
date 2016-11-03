4c30fdccfc179efc23566c5be217558c63ab6641
Nick Parsons <nparsons08@gmail.com>
2016-11-03 16:18:27 -0600

    Footer fix

--- assets/js/components/Footer/index.js
+++ assets/js/components/Footer/index.js
@@ -13,7 +13,7 @@ class Footer extends Component {
                     <div className="row">
                         <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9 text-left">
                             <em>
-                                RSS Reader was built by <a href="https://getstream.io" target="_blank">Stream</a>
+                                Winds was built by <a href="https://getstream.io" target="_blank">Stream</a>
                                 &nbsp;|&nbsp;
                                 Powered with <a href="https://getstream.io/docs_analytics/#introductionp" target="_blank">Personalization</a>
                                 &nbsp;|&nbsp;
