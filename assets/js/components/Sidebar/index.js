e36fe0e7f5f81cd5d059ab40c9b38773c6223d03
Nick Parsons <nparsons08@gmail.com>
2016-11-14 10:19:41 -0700

    Update index.js

--- assets/js/components/Sidebar/index.js
+++ assets/js/components/Sidebar/index.js
@@ -167,7 +167,7 @@ class Sidebar extends Component {
                                     minLength={6}
                                     className="form-control"
                                     id="password"
-                                    placeholder="Password at least 6 characters long" />
+                                    placeholder="Minimum of 6 characters" />
                             </div>
                             <button type="submit" className="btn">Update</button>
                             <button type="button" className="btn" onClick={() => this.setState({ profile: false, })}>Cancel</button>
