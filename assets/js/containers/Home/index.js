a0f4c05ba4b1ff8b3f069f0b238cfa81883d95d7
Nick Parsons <nparsons08@gmail.com>
2016-11-18 11:34:59 -0700

    Temporarily removing realtime functionality.

--- assets/js/containers/Home/index.js
+++ assets/js/containers/Home/index.js
@@ -34,15 +34,15 @@ class Home extends Component {
 
         this.props.dispatch(FeedActions.load())
 
-        let notification = notify.feed(
-            'timeline',
-            localStorage.getItem('id'),
-            localStorage.getItem('timeline_token')
-        )
-
-        notification.subscribe((data) => {
-            this.setState({ unread: this.state.unread + 1, })
-        })
+        // let notification = notify.feed(
+        //     'timeline',
+        //     localStorage.getItem('id'),
+        //     localStorage.getItem('timeline_token')
+        // )
+        //
+        // notification.subscribe((data) => {
+        //     this.setState({ unread: this.state.unread + 1, })
+        // })
 
     }
 
