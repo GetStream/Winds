c14dce13c311147aed2da19dfad060055ea53bd4
Nick Parsons <nparsons08@gmail.com>
2016-11-21 13:49:36 -0700

    Realtime

--- assets/js/containers/Home/index.js
+++ assets/js/containers/Home/index.js
@@ -33,17 +33,19 @@ class Home extends Component {
     )
 
     componentWillMount() {
+        
         this.props.dispatch(FeedActions.load())
 
-        // let notification = realtime.feed(
-        //     'timeline',
-        //     localStorage.getItem('id'),
-        //     localStorage.getItem('timeline_token')
-        // )
-        //
-        // notification.subscribe((data) => {
-        //     this.setState({ unread: this.state.unread + 1, })
-        // })
+        let notification = realtime.feed(
+            'timeline',
+            localStorage.getItem('id'),
+            localStorage.getItem('timeline_token')
+        )
+
+        notification.subscribe((data) => {
+            this.setState({ unread: this.state.unread + 1, })
+        })
+
     }
 
     componentWillReceiveProps(props) {
