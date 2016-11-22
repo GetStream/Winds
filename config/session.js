d534e29bce2816ef3bd66cfb86a827e93d1b7412
Nick Parsons <nparsons08@gmail.com>
2016-11-22 08:58:09 -0700

    Redis authentication

--- config/session.js
+++ config/session.js
@@ -30,18 +30,18 @@ module.exports.session = {
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
-  // adapter: 'redis',
-  //
+  adapter: 'redis',
+
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
   // Read more about options at: https://github.com/visionmedia/connect-redis
-  //
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
-  // prefix: 'sess:'
+
+  host: 'localhost',
+  port: 6379,
+  //ttl: <redis session TTL in seconds>,
+  db: 0,
+  pass: process.env.REDIS_AUTH,
+  prefix: 'sess:'
 
 
   // Uncomment the following lines to use your Mongo adapter as a session store
