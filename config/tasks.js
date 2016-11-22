1dd9d3bdab1ed79b6009e9c68298bc07deda066b
Nick Parsons <nparsons08@gmail.com>
2016-11-22 14:37:47 -0700

    Tasks update

--- config/tasks.js
+++ config/tasks.js
@@ -2,7 +2,7 @@ module.exports.tasks = {
     // see https://github.com/Automattic/kue#redis-connection-settings
     redis: {
         port: 6379,
-        host: 'https://winds.getstream.io',
+        host: 'http://winds.getstream.io',
         auth: process.env.REDIS_AUTH,
         db: 0, // if provided select a non-default redis db
         // options: {
