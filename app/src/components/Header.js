427beafe9f806d68c70e23649f363ede5ea7e7f1
Nick Parsons <nparsons08@gmail.com>
2018-05-02 15:27:35 -0600

    Tests to see if the binary is running on Windows.

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -15,6 +15,7 @@ import { connect } from 'react-redux';
 import Popover from 'react-popover';
 import AddRSSModal from './AddRSSModal';
 import AddPodcastModal from './AddPodcastModal';
+import windows from 'is-windows';
 
 class Header extends Component {
 	constructor(props) {
@@ -160,13 +161,21 @@ class Header extends Component {
 			</div>
 		);
 
-		let header = (
-			<header className={'header'}>
+		let title = null;
+
+		if (!windows()) {
+			title = (
 				<div className="title">
 					<a href="https://getstream.io/?utm_source=Winds&utm_medium=Winds&utm_content=winds_homepage">
 						Winds 2.0 – Powered by GetStream.io
 					</a>
 				</div>
+			);
+		}
+
+		let header = (
+			<header className={'header'}>
+				{title}
 				<div className="header-content">
 					<div className="left">
 						<div className="nav">
