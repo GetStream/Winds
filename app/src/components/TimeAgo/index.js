28320af85c6e13240c206e34e44bff0b90a4dcf3
Ken Hoff <ken@hoff.tech>
2018-05-10 16:16:50 -0600

    support for trimming time ago from moment

--- app/src/components/TimeAgo/index.js
+++ app/src/components/TimeAgo/index.js
@@ -4,11 +4,16 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 
 const TimeAgo = props => {
-	return <span>{moment.utc(props.timestamp).fromNow()}</span>;
+	return <span>{moment.utc(props.timestamp).fromNow(props.trim)}</span>;
+};
+
+TimeAgo.defaultProps = {
+	trim: false,
 };
 
 TimeAgo.propTypes = {
 	timestamp: PropTypes.oneOfType([PropTypes.instanceOf(Date), PropTypes.string]),
+	trim: PropTypes.bool,
 };
 
 export default TimeAgo;
