aad3ca8ebbbe8843777c8ad4b44efe9da0eab087
Ken Hoff <ken@hoff.tech>
2018-05-10 17:31:52 -0600

    bookmarks components

---
+++ app/src/components/PodcastPanels/BookmarkedEpisodes.js
@@ -0,0 +1,92 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import fetch from '../../util/fetch';
+import Panel from '../Panel';
+import { Link } from 'react-router-dom';
+import getPlaceholderImageURL from '../../util/getPlaceholderImageURL';
+import Img from 'react-image';
+import TimeAgo from '../TimeAgo';
+
+class BookmarkedEpisodes extends React.Component {
+	componentDidMount() {
+		fetch('GET', '/pins', null, { type: 'episode' }).then(response => {
+			for (let pin of response.data) {
+				// dispatch update to podcast
+				this.props.dispatch({
+					podcast: pin.episode.podcast,
+					type: 'UPDATE_PODCAST_SHOW',
+				});
+				// dispatch updates to episodes
+				this.props.dispatch({
+					episode: pin.episode,
+					type: 'UPDATE_EPISODE',
+				});
+				// dispatch updates to pins
+				this.props.dispatch({
+					pin,
+					type: 'PIN_EPISODE',
+				});
+			}
+		});
+	}
+	render() {
+		return (
+			<Panel>
+				<Panel.Header>Bookmarks</Panel.Header>
+				<Panel.Contents>
+					{this.props.bookmarks.map(bookmark => {
+						return (
+							<Link
+								key={bookmark._id}
+								to={`/podcasts/${bookmark.episode.podcast._id}`}
+							>
+								<Img
+									src={[
+										bookmark.episode.podcast.images.favicon,
+										getPlaceholderImageURL(
+											bookmark.episode.podcast._id,
+										),
+									]}
+								/>
+								<div>{bookmark.episode.title}</div>
+								<TimeAgo
+									className="muted"
+									timestamp={bookmark.episode.publicationDate}
+									trim={true}
+								/>
+							</Link>
+						);
+					})}
+				</Panel.Contents>
+			</Panel>
+		);
+	}
+}
+
+BookmarkedEpisodes.defaultProps = {
+	bookmarks: [],
+};
+
+BookmarkedEpisodes.propTypes = {
+	bookmarks: PropTypes.arrayOf(PropTypes.shape()),
+	dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state, ownProps) => {
+	let bookmarks = [];
+	for (var episodeID in state.pinnedEpisodes) {
+		if (state.pinnedEpisodes.hasOwnProperty(episodeID)) {
+			if (state.pinnedEpisodes[episodeID]) {
+				let pin = { ...state.pinnedEpisodes[episodeID] };
+				pin.episode = { ...state.episodes[episodeID] };
+				pin.episode.podcast = { ...state.podcasts[pin.episode.podcast] };
+				bookmarks.push(pin);
+			}
+		}
+	}
+
+	return { ...ownProps, bookmarks };
+};
+
+export default connect(mapStateToProps)(BookmarkedEpisodes);
---
+++ app/src/components/RSSPanels/BookmarkedArticles.js
@@ -0,0 +1,98 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import fetch from '../../util/fetch';
+import Panel from '../Panel';
+import { Link } from 'react-router-dom';
+import getPlaceholderImageURL from '../../util/getPlaceholderImageURL';
+import Img from 'react-image';
+import TimeAgo from '../TimeAgo';
+
+class BookmarkedArticles extends React.Component {
+	componentDidMount() {
+		fetch('GET', '/pins', null, {
+			type: 'article',
+			user: localStorage['authedUser'],
+		}).then(response => {
+			for (let pin of response.data) {
+				// dispatch update to rss feeds
+				this.props.dispatch({
+					rssFeed: pin.article.rss,
+					type: 'UPDATE_RSS_FEED',
+				});
+				// dispatch updates to articles
+				this.props.dispatch({
+					rssArticle: { ...pin.article, type: 'article' },
+					type: 'UPDATE_ARTICLE',
+				});
+				// dispatch updates to pins
+				this.props.dispatch({
+					pin,
+					type: 'PIN_ARTICLE',
+				});
+			}
+		});
+	}
+	render() {
+		return (
+			<Panel>
+				<Panel.Header>Bookmarks</Panel.Header>
+				<Panel.Contents>
+					{this.props.bookmarks.map(bookmark => {
+						return (
+							<Link
+								key={bookmark._id}
+								to={`/rss/${bookmark.article.rss._id}/articles/${
+									bookmark.article._id
+								}`}
+							>
+								<Img
+									src={[
+										bookmark.article.rss.images.favicon,
+										getPlaceholderImageURL(bookmark.article.rss._id),
+									]}
+								/>
+								<div>{bookmark.article.title}</div>
+								<TimeAgo
+									className="muted"
+									timestamp={bookmark.article.publicationDate}
+									trim={true}
+								/>
+							</Link>
+						);
+					})}
+				</Panel.Contents>
+			</Panel>
+		);
+	}
+}
+
+BookmarkedArticles.defaultProps = {
+	bookmarks: [],
+};
+
+BookmarkedArticles.propTypes = {
+	bookmarks: PropTypes.arrayOf(PropTypes.shape()),
+	dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state, ownProps) => {
+	let bookmarks = [];
+	for (var articleID in state.pinnedArticles) {
+		if (state.pinnedArticles.hasOwnProperty(articleID)) {
+			if (state.pinnedArticles[articleID]) {
+				let pin = { ...state.pinnedArticles[articleID] };
+				pin.article = { ...state.articles[articleID] };
+				pin.article.rss = { ...state.rssFeeds[pin.article.rss] };
+				bookmarks.push(pin);
+			}
+		}
+	}
+
+	return {
+		...ownProps,
+		bookmarks,
+	};
+};
+
+export default connect(mapStateToProps)(BookmarkedArticles);
