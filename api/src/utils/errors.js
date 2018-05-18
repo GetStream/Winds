eb8b361f1f9a82f9119cb67c8a5ae1102f16f161
Thierry Schellenbach <thierryschellenbach@gmail.com>
2018-05-18 14:00:44 -0600

    missing import

--- api/src/utils/errors.js
+++ api/src/utils/errors.js
@@ -1,6 +1,8 @@
 import config from '../config';
 import winston from 'winston';
 import Raven from 'raven';
+import path from 'path';
+
 let readmePath = require.resolve("raven");
 
 const executable = path.basename(process.argv[1]);
