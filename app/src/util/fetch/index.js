9432b83338ef90fd1774146991585ad92fcf7b1d
Ken Hoff <ken@hoff.tech>
2018-05-19 14:48:02 -0600

    'fetch' utility now returns axios directly

--- app/src/util/fetch/index.js
+++ app/src/util/fetch/index.js
@@ -2,35 +2,27 @@ import axios from 'axios';
 import config from '../../config';
 
 const fetch = (method, path, data, params) => {
-	return new Promise((resolve, reject) => {
-		if (!method) {
-			reject(new Error('Method is a required field.'));
-		}
+	if (!method) {
+		throw new Error('Method is a required field.');
+	}
 
-		if (!path) {
-			reject(new Error('Path is a required field.'));
-		}
+	if (!path) {
+		throw new Error('Path is a required field.');
+	}
 
-		let options = {
-			baseURL: config.api.url,
-			data: data || {},
-			headers: {
-				'Authorization': `Bearer ${localStorage['jwt']}`,
-				'Content-Type': 'application/json',
-			},
-			method: method.toUpperCase(),
-			params: params || {},
-			url: path,
-		};
+	let options = {
+		baseURL: config.api.url,
+		data: data || {},
+		headers: {
+			'Authorization': `Bearer ${localStorage['jwt']}`,
+			'Content-Type': 'application/json',
+		},
+		method: method.toUpperCase(),
+		params: params || {},
+		url: path,
+	};
 
-		axios(options)
-			.then(res => {
-				resolve(res);
-			})
-			.catch(err => {
-				reject(err);
-			});
-	});
+	return axios(options);
 };
 
 export default fetch;
