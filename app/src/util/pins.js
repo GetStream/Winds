1eed0c78687ec4789037e113ba9b3adbfc7085c7
Ken Hoff <ken@hoff.tech>
2018-05-21 17:45:58 -0600

    cleaning up comments

--- app/src/util/pins.js
+++ app/src/util/pins.js
@@ -33,24 +33,6 @@ const getPinnedArticles = dispatch => {
 		type: 'article',
 		user: localStorage['authedUser'],
 	}).then(response => {
-		// for (let pin of response.data) {
-		// 	// dispatch update to rss feeds
-		// 	dispatch({
-		// 		rssFeed: pin.article.rss,
-		// 		type: 'UPDATE_RSS_FEED',
-		// 	});
-		// 	// dispatch updates to articles
-		// 	dispatch({
-		// 		rssArticle: { ...pin.article, type: 'article' },
-		// 		type: 'UPDATE_ARTICLE',
-		// 	});
-		// 	// dispatch updates to pins
-		// 	dispatch({
-		// 		pin,
-		// 		type: 'PIN_ARTICLE',
-		// 	});
-		// }
-
 		let rssFeeds = [];
 		let articles = [];
 		for (let pin of response.data) {
