8abe79a3382679d76773c382d37a44508035b048
Thierry Schellenbach <thierryschellenbach@gmail.com>
2018-05-26 09:43:48 -0600

    typo

--- api/src/workers/og.js
+++ api/src/workers/og.js
@@ -43,7 +43,7 @@ async function handleOg(job) {
 
 async function isValidUrl(url) {
     let invalid = invalidExtensions.some(extension=> {
-      if( url.endsWith(`.${extensions}`)) {
+      if( url.endsWith(`.${extension}`)) {
         return extension
       }
     })
