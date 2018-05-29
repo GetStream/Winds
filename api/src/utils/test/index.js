49e6a3f6f67d6a3db397df9f79e9c07d542c07fc
Max Klyga <max@getstream.io>
2018-05-29 02:00:13 +0200

    Add basic signup tests

--- api/src/utils/test/index.js
+++ api/src/utils/test/index.js
@@ -1,10 +1,16 @@
 import sinon from 'sinon'
 import rewiremock from 'rewiremock'
 import stream from 'getstream'
+import StreamFeed from 'getstream/src/lib/feed'
 
 export const mockClient = sinon.createStubInstance(stream.Client);
 
 export function setupMocks() {
+    const mockFeed = sinon.createStubInstance(StreamFeed);
+    mockFeed.follow.returns(Promise.resolve());
+
+    mockClient.feed.returns(mockFeed);
+
     rewiremock('getstream').with({ connect: sinon.stub().returns(mockClient) });
     rewiremock('../events').with(sinon.stub().returns(Promise.resolve()));
 
--- api/test/controllers/auth.js
+++ api/test/controllers/auth.js
@@ -1,23 +1,15 @@
 import { expect, request } from 'chai'
 import sinon from 'sinon'
-import StreamFeed from 'getstream/src/lib/feed'
 
 import api from '../../src/server'
 import auth from '../../src/controllers/auth'
 import User from '../../src/models/user'
 
- import { mockClient } from '../../src/utils/test'
-
 describe('Auth controller', () => {
     describe('signup', () => {
-        beforeEach(async () => { await User.remove().exec(); })
+        beforeEach(() => User.remove().exec())
 
         it('should return 200 for valid request', async () => {
-            const mockFeed = sinon.createStubInstance(StreamFeed);
-            mockFeed.follow.returns(Promise.resolve());
-
-            mockClient.feed.returns(mockFeed);
-
             const response = await request(api).post('/auth/signup').send({
                 email: 'valid@email.com',
                 username: 'valid',
@@ -26,6 +18,55 @@ describe('Auth controller', () => {
             });
 
             expect(response).to.have.status(200);
+        })
+
+        it('should return 422 for missing data in request', async () => {
+            const bodies = [
+                { username: 'valid', name: 'Valid Name', password: 'valid_password' },
+                { email: 'valid@email.com', name: 'Valid Name', password: 'valid_password' },
+                { email: 'valid@email.com', username: 'valid', password: 'valid_password' },
+                { email: 'valid@email.com', username: 'valid', name: 'Valid Name' }
+            ];
+            const requests = bodies.map((body) => request(api).post('/auth/signup').send(body));
+            for await (const response of requests) {
+                expect(response).to.have.status(422);
+            }
+        })
+
+        it('should return 422 for invalid email in request', async () => {
+            const bodies = [
+                { email: 'invalid.email.com', username: 'valid', name: 'Valid Name', password: 'valid_password' },
+                { email: 'invalid@email', username: 'valid', name: 'Valid Name', password: 'valid_password' },
+                { email: '@invalid.email.com', username: 'valid', name: 'Valid Name', password: 'valid_password' },
+            ];
+            const requests = bodies.map((body) => request(api).post('/auth/signup').send(body));
+            for await (const response of requests) {
+                expect(response).to.have.status(422);
+            }
+        })
+
+        it('should return 422 for invalid username in request', async () => {
+            const response = await request(api).post('/auth/signup').send({
+                email: 'valid@email.com',
+                username: 'invalid-username',
+                name: 'Valid Name',
+                password: 'valid_password'
+            });
+
+            expect(response).to.have.status(422);
+        })
+
+        it('should return 409 for existing user', async () => {
+            const user = {
+                email: 'valid@email.com',
+                username: 'valid',
+                name: 'Valid Name',
+                password: 'valid_password'
+            };
+            await User.create(user);
+            const response = await request(api).post('/auth/signup').send(user);
+
+            expect(response).to.have.status(409);
         })
     })
 })
