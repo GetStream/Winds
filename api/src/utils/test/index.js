1f1a0a074bcf013b7656078313f48a13979eb038
Max Klyga <max@getstream.io>
2018-05-29 16:32:55 +0200

    Add fixture loading utilities. Add basic login tests

--- api/src/utils/test/index.js
+++ api/src/utils/test/index.js
@@ -1,18 +1,41 @@
 import sinon from 'sinon'
+import bcrypt from 'bcryptjs'
 import rewiremock from 'rewiremock'
 import stream from 'getstream'
-import StreamFeed from 'getstream/src/lib/feed'
+import mongoose from 'mongoose'
 
 export const mockClient = sinon.createStubInstance(stream.Client);
 
 export function setupMocks() {
-    const mockFeed = sinon.createStubInstance(StreamFeed);
-    mockFeed.follow.returns(Promise.resolve());
-
-    mockClient.feed.returns(mockFeed);
+    mockClient.feed.callsFake((group, id) => {
+        return {
+            slug: group,
+            userId: id,
+            id: group + ':' + id,
+            follow: sinon.stub().returns(Promise.resolve())
+        };
+    });
 
     rewiremock('getstream').with({ connect: sinon.stub().returns(mockClient) });
     rewiremock('../events').with(sinon.stub().returns(Promise.resolve()));
 
     rewiremock.enable();
+}
+
+export async function loadFixture(fixture) {
+    const filters = {
+        'User': async (user) => {
+            user.password = await bcrypt.hash(user.password, 8)
+            return user;
+        }
+    };
+    const models = require(`../../../test/fixtures/${fixture}.json`);
+
+    for (const modelName in models) {
+        const model = mongoose.model(modelName);
+        const filter = filters[modelName] || Promise.resolve;
+        const filteredData = await Promise.all(models[modelName].map(filter));
+
+        await model.collection.insertMany(filteredData);
+    }
 }
--- api/test/controllers/auth.js
+++ api/test/controllers/auth.js
@@ -1,9 +1,9 @@
 import { expect, request } from 'chai'
-import sinon from 'sinon'
 
 import api from '../../src/server'
 import auth from '../../src/controllers/auth'
 import User from '../../src/models/user'
+import { loadFixture } from '../../src/utils/test'
 
 describe('Auth controller', () => {
     describe('signup', () => {
@@ -20,12 +20,16 @@ describe('Auth controller', () => {
             expect(response).to.have.status(200);
         })
 
-        it('should return 422 for missing data in request', async () => {
+        it('should return 422 for missing/empty data in request', async () => {
             const bodies = [
                 { username: 'valid', name: 'Valid Name', password: 'valid_password' },
                 { email: 'valid@email.com', name: 'Valid Name', password: 'valid_password' },
                 { email: 'valid@email.com', username: 'valid', password: 'valid_password' },
-                { email: 'valid@email.com', username: 'valid', name: 'Valid Name' }
+                { email: 'valid@email.com', username: 'valid', name: 'Valid Name' },
+                { email: '', username: 'valid', name: 'Valid Name', password: 'valid_password' },
+                { email: 'invalid.email.com', username: '', name: 'Valid Name', password: 'valid_password' },
+                { email: 'invalid.email.com', username: 'valid', name: '', password: 'valid_password' },
+                { email: 'invalid.email.com', username: 'valid', name: 'Valid Name', password: '' }
             ];
             const requests = bodies.map((body) => request(api).post('/auth/signup').send(body));
             for await (const response of requests) {
@@ -57,16 +61,63 @@ describe('Auth controller', () => {
         })
 
         it('should return 409 for existing user', async () => {
-            const user = {
+            await loadFixture('example')
+
+            const response = await request(api).post('/auth/signup').send({
                 email: 'valid@email.com',
                 username: 'valid',
                 name: 'Valid Name',
                 password: 'valid_password'
-            };
-            await User.create(user);
-            const response = await request(api).post('/auth/signup').send(user);
+            });
 
             expect(response).to.have.status(409);
+        })
+    })
+
+    describe('login', () => {
+        before(async () => {
+            await User.remove().exec();
+            await loadFixture('example');
+        })
+
+        it('should return 200 for existing user', async () => {
+            const response = await request(api).post('/auth/login').send({
+                email: 'valid@email.com',
+                password: 'valid_password'
+            });
+
+            expect(response).to.have.status(200);
+        })
+
+        it('should return 401 for missing/empty data in request', async () => {
+            const bodies = [
+                { password: 'valid_password' },
+                { email: 'valid@email.com' },
+                { email: '', password: 'valid_password' },
+                { email: 'valid@email.com', password: '' },
+            ];
+            const requests = bodies.map((body) => request(api).post('/auth/login').send(body));
+            for await (const response of requests) {
+                expect(response).to.have.status(401);
+            }
+        })
+
+        it('should return 404 for nonexistent user', async () => {
+            const response = await request(api).post('/auth/login').send({
+                email: 'invalid@email.com',
+                password: 'valid_password'
+            });
+
+            expect(response).to.have.status(404);
+        })
+
+        it('should return 403 for existing user w/ wrong password', async () => {
+            const response = await request(api).post('/auth/login').send({
+                email: 'valid@email.com',
+                password: 'invalid_password'
+            });
+
+            expect(response).to.have.status(403);
         })
     })
 })
