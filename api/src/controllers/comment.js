41748770b3f23413f19756f73a36ad5270c6ada4
Ken Hoff <ken@hoff.tech>
2018-05-30 11:44:30 -0600

    linting api controllers

--- api/src/controllers/comment.js
+++ api/src/controllers/comment.js
@@ -1,99 +1,99 @@
-import Comment from '../models/comment'
-import Share from '../models/share'
+import Comment from '../models/comment';
+import Share from '../models/share';
 
-import config from '../config'
-import logger from '../utils/logger'
+import config from '../config';
+import logger from '../utils/logger';
 
 exports.list = (req, res) => {
-    Comment.apiQuery(req.query)
-        .then(comments => {
-            res.json(comments)
-        })
-        .catch(err => {
-            logger.error(err)
-            res.status(422).send(err.errors)
-        })
-}
+	Comment.apiQuery(req.query)
+		.then(comments => {
+			res.json(comments);
+		})
+		.catch(err => {
+			logger.error(err);
+			res.status(422).send(err.errors);
+		});
+};
 
 exports.get = (req, res) => {
-    if (req.params.commentId == 'undefined') {
-        return res.sendStatus(404)
-    }
+	if (req.params.commentId == 'undefined') {
+		return res.sendStatus(404);
+	}
 
-    Comment.findById(req.params.commentId)
-        .then(comment => {
-            if (!comment) {
-                return res.sendStatus(404)
-            }
+	Comment.findById(req.params.commentId)
+		.then(comment => {
+			if (!comment) {
+				return res.sendStatus(404);
+			}
 
-            res.json(comment)
-        })
-        .catch(err => {
-            logger.error(err)
-            res.status(422).send(err.errors)
-        })
-}
+			res.json(comment);
+		})
+		.catch(err => {
+			logger.error(err);
+			res.status(422).send(err.errors);
+		});
+};
 
 exports.post = (req, res) => {
-    const data = Object.assign(req.body, { user: req.user.sub }) || {}
+	const data = Object.assign(req.body, { user: req.user.sub }) || {};
 
-    // create the comment
-    // update the comment counter on the share
-    Promise.all([
-        Comment.create(data),
-        Share.findByIdAndUpdate(
-            {
-                _id: data.share,
-            },
-            { $inc: { comments: 1 } },
-            {
-                new: false,
-                upsert: false,
-            },
-        ),
-    ])
-        .then(results => {
-            res.json(results[0]) // send back created comment
-        })
-        .catch(err => {
-            logger.error(err)
-            return res.status(422).send(err)
-        })
-}
+	// create the comment
+	// update the comment counter on the share
+	Promise.all([
+		Comment.create(data),
+		Share.findByIdAndUpdate(
+			{
+				_id: data.share,
+			},
+			{ $inc: { comments: 1 } },
+			{
+				new: false,
+				upsert: false,
+			},
+		),
+	])
+		.then(results => {
+			res.json(results[0]); // send back created comment
+		})
+		.catch(err => {
+			logger.error(err);
+			return res.status(422).send(err);
+		});
+};
 
 exports.put = (req, res) => {
-    const data = req.body || {}
-    let opts = {
-        new: true,
-    }
+	const data = req.body || {};
+	let opts = {
+		new: true,
+	};
 
-    // need to only allow updating if user in jwt matches user on comment
-    // OR, if the only thing that's being updated is the flags count
+	// need to only allow updating if user in jwt matches user on comment
+	// OR, if the only thing that's being updated is the flags count
 
-    Comment.findById({ _id: req.params.commentId })
-        .then(comment => {
-            if (!comment) {
-                return res.sendStatus(404)
-            } else {
-                // if it's just updating the flags, or the user matches, update the comment
-                if (data.flags || comment.user === res.sub.user) {
-                    Comment.findByIdAndUpdate(
-                        { _id: req.params.commentId },
-                        data,
-                        opts,
-                    ).then(comment => {
-                        return res.json(comment)
-                    })
-                } else {
-                    // else, unauthorized
-                    return res
-                        .status(401)
-                        .send("You're not authorized to edit this comment.")
-                }
-            }
-        })
-        .catch(err => {
-            logger.error(err)
-            res.status(422).send(err.errors)
-        })
-}
+	Comment.findById({ _id: req.params.commentId })
+		.then(comment => {
+			if (!comment) {
+				return res.sendStatus(404);
+			} else {
+				// if it's just updating the flags, or the user matches, update the comment
+				if (data.flags || comment.user === res.sub.user) {
+					Comment.findByIdAndUpdate(
+						{ _id: req.params.commentId },
+						data,
+						opts,
+					).then(comment => {
+						return res.json(comment);
+					});
+				} else {
+					// else, unauthorized
+					return res
+						.status(401)
+						.send('You\'re not authorized to edit this comment.');
+				}
+			}
+		})
+		.catch(err => {
+			logger.error(err);
+			res.status(422).send(err.errors);
+		});
+};
--- api/src/controllers/listen.js
+++ api/src/controllers/listen.js
@@ -1,98 +1,98 @@
-import Listen from "../models/listen"
-import User from "../models/user"
+import Listen from '../models/listen';
+import User from '../models/user';
 
-import config from "../config"
-import logger from "../utils/logger"
-import events from "../utils/events"
+import config from '../config';
+import logger from '../utils/logger';
+import events from '../utils/events';
 
 exports.list = (req, res) => {
-    Listen.apiQuery(req.query)
-        .then(listens => {
-            res.json(listens)
-        })
-        .catch(err => {
-            logger.error(err)
-            res.status(422).send(err.errors)
-        })
-}
+	Listen.apiQuery(req.query)
+		.then(listens => {
+			res.json(listens);
+		})
+		.catch(err => {
+			logger.error(err);
+			res.status(422).send(err.errors);
+		});
+};
 
 exports.get = (req, res) => {
-    if (req.params.listenId == "undefined") {
-        return res.sendStatus(404)
-    }
+	if (req.params.listenId == 'undefined') {
+		return res.sendStatus(404);
+	}
 
-    Listen.findById(req.params.listenId)
-        .then(listen => {
-            if (!listen) {
-                return res.sendStatus(404)
-            }
+	Listen.findById(req.params.listenId)
+		.then(listen => {
+			if (!listen) {
+				return res.sendStatus(404);
+			}
 
-            res.json(listen)
-        })
-        .catch(err => {
-            logger.error(err)
-            res.status(422).send(err.errors)
-        })
-}
+			res.json(listen);
+		})
+		.catch(err => {
+			logger.error(err);
+			res.status(422).send(err.errors);
+		});
+};
 
 exports.post = (req, res) => {
-    const data = Object.assign({}, req.body, { user: req.user.sub }) || {}
+	const data = Object.assign({}, req.body, { user: req.user.sub }) || {};
 
-    Listen.findOneAndUpdate(
-        { user: data.user, episode: data.episode },
-        { $set: data },
-        { new: true, upsert: true },
-    )
-        .then(listen => {
-            User.findById(data.user)
-                .then(user => {
-                    events({
-                        user: user._id,
-                        email: user.email.toLowerCase(),
-                        engagement: {
-                            label: "listen",
-                            content: {
-                                foreign_id: `episode:${listen.episode}`,
-                                duration: listen.duration,
-                            },
-                        },
-                    })
-                        .then(() => {
-                            res.json(listen)
-                        })
-                        .catch(err => {
-                            logger.error(err)
-                            res.sendStatus(503)
-                        })
-                })
-                .catch(err => {
-                    logger.error(err)
-                    res.sendStatus(422)
-                })
-        })
-        .catch(err => {
-            logger.error(err)
-            res.status(422).send(err.errors)
-        })
-}
+	Listen.findOneAndUpdate(
+		{ user: data.user, episode: data.episode },
+		{ $set: data },
+		{ new: true, upsert: true },
+	)
+		.then(listen => {
+			User.findById(data.user)
+				.then(user => {
+					events({
+						user: user._id,
+						email: user.email.toLowerCase(),
+						engagement: {
+							label: 'listen',
+							content: {
+								foreign_id: `episode:${listen.episode}`,
+								duration: listen.duration,
+							},
+						},
+					})
+						.then(() => {
+							res.json(listen);
+						})
+						.catch(err => {
+							logger.error(err);
+							res.sendStatus(503);
+						});
+				})
+				.catch(err => {
+					logger.error(err);
+					res.sendStatus(422);
+				});
+		})
+		.catch(err => {
+			logger.error(err);
+			res.status(422).send(err.errors);
+		});
+};
 
 exports.delete = (req, res) => {
-    Listen.findById(req.params.listenId)
-        .then(listen => {
-            if (!listen) {
-                res.status(404).send()
-                return
-            } else if (listen.user !== req.user.sub) {
-                res.status(401).send("You aren't authorized to delete that listen.")
-                return
-            } else {
-                return Listen.remove({ _id: req.params.listenId }).then(() => {
-                    res.status(204).send()
-                })
-            }
-        })
-        .catch(err => {
-            logger.error(err)
-            res.status(422).send(err.errors)
-        })
-}
+	Listen.findById(req.params.listenId)
+		.then(listen => {
+			if (!listen) {
+				res.status(404).send();
+				return;
+			} else if (listen.user !== req.user.sub) {
+				res.status(401).send('You aren\'t authorized to delete that listen.');
+				return;
+			} else {
+				return Listen.remove({ _id: req.params.listenId }).then(() => {
+					res.status(204).send();
+				});
+			}
+		})
+		.catch(err => {
+			logger.error(err);
+			res.status(422).send(err.errors);
+		});
+};
