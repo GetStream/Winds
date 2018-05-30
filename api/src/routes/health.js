87a0ffd0004d3d114c8844d463f1d25856667bc1
Tommaso Barbugli <tbarbugli@gmail.com>
2018-05-30 20:12:41 +0200

    add helper to wrap async controller fn

--- api/src/routes/health.js
+++ api/src/routes/health.js
@@ -1,14 +1,10 @@
 import Health from "../controllers/health"
+import { wrapAsync } from "../utils/controllers"
 
-const asyncMiddleware = fn =>
-	(req, res, next) => {
-		Promise.resolve(fn(req, res, next))
-			.catch(next);
-	};
 
 module.exports = api => {
-    api.route("/health").get(Health.health)
-    api.route("/status").get(Health.status)
-    api.route("/sentry/log").get(asyncMiddleware(Health.sentryLog))
-    api.route("/sentry/throw").get(asyncMiddleware(Health.sentryThrow))
+    api.route("/health").get(wrapAsync(Health.health))
+    api.route("/status").get(wrapAsync(Health.status))
+    api.route("/sentry/log").get(wrapAsync(Health.sentryLog))
+    api.route("/sentry/throw").get(wrapAsync(Health.sentryThrow))
 }
