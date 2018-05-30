56610c53c5eabcc83e730f14a3fcb90b18213280
Ken Hoff <ken@hoff.tech>
2018-05-30 13:18:14 -0600

    linting top-level api files

--- api/src/server.js
+++ api/src/server.js
@@ -1,75 +1,75 @@
-import fs from "fs"
-import path from "path"
-import express from "express"
-import bodyParser from "body-parser"
-import cors from "cors"
-import compression from "compression"
-import jwt from "express-jwt"
-import limit from "express-rate-limit"
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import compression from 'compression';
+import jwt from 'express-jwt';
+import limit from 'express-rate-limit';
 
-import config from "./config"
-import logger from "./utils/logger"
+import config from './config';
+import logger from './utils/logger';
 
-const api = express()
+const api = express();
 
-api.use(cors())
-api.use(compression())
-api.use(bodyParser.urlencoded({ extended: true }))
-api.use(bodyParser.json())
+api.use(cors());
+api.use(compression());
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(bodyParser.json());
 
-api.enable("trust proxy")
+api.enable('trust proxy');
 api.use(
-    new limit({
-        windowMs: 60 * 1000,
-        max: 100,
-        delayMs: 0,
-    }),
-)
+	new limit({
+		windowMs: 60 * 1000,
+		max: 100,
+		delayMs: 0,
+	}),
+);
 
-api.set("json spaces", 4)
+api.set('json spaces', 4);
 
 api.use(
-    jwt({ secret: config.jwt.secret }).unless({
-        path: [
-            "/",
-            "/health",
-            "/status",
-            "/auth/signup",
-            "/auth/login",
-            "/auth/forgot-password",
-            "/auth/reset-password",
-        ],
-    }),
-)
+	jwt({ secret: config.jwt.secret }).unless({
+		path: [
+			'/',
+			'/health',
+			'/status',
+			'/auth/signup',
+			'/auth/login',
+			'/auth/forgot-password',
+			'/auth/reset-password',
+		],
+	}),
+);
 
 api.use((err, req, res, next) => {
-    if (err.name === "UnauthorizedError") {
-        res.status(401).send("Missing authentication credentials.")
-    }
-})
+	if (err.name === 'UnauthorizedError') {
+		res.status(401).send('Missing authentication credentials.');
+	}
+});
 
 api.use((req, res, next) => {
-    res.setHeader("X-Powered-By", "Winds - Powered by GetStream.io")
-    next()
-})
+	res.setHeader('X-Powered-By', 'Winds - Powered by GetStream.io');
+	next();
+});
 
 if (require.main === module) {
-    api.listen(config.server.port, err => {
-        if (err) {
-            logger.error(err)
-            process.exit(1)
-        }
+	api.listen(config.server.port, err => {
+		if (err) {
+			logger.error(err);
+			process.exit(1);
+		}
 
-        require("./utils/db")
+		require('./utils/db');
 
-        fs.readdirSync(path.join(__dirname, "routes")).map(file => {
-            if (file.endsWith(".js")) {
-                require("./routes/" + file)(api)
-            }
-        })
+		fs.readdirSync(path.join(__dirname, 'routes')).map(file => {
+			if (file.endsWith('.js')) {
+				require('./routes/' + file)(api);
+			}
+		});
 
-        logger.info(`API is now running on port ${config.server.port} in ${config.env} mode`)
-    })
+		logger.info(`API is now running on port ${config.server.port} in ${config.env} mode`);
+	});
 }
 
-module.exports = api
+module.exports = api;
