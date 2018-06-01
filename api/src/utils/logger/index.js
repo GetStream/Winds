98e8b554dbc78e79ed1039ea6b9b92f4382a5e38
Tommaso Barbugli <tbarbugli@gmail.com>
2018-06-01 16:49:25 +0200

    fix logging and error reporting for og and rss workers

--- api/src/utils/logger/index.js
+++ api/src/utils/logger/index.js
@@ -2,24 +2,46 @@ import config from '../../config';
 import winston from 'winston';
 import { createSentryTransport } from './sentry';
 import { Raven } from '../errors';
-import { format } from 'logform';
+const { format } = require('winston');
 
-// https://github.com/guzru/winston-sentry
-const transports = [new winston.transports.Console({ level: 'silly' })];
+const warnAboutWinston = format((info) => {
+	if (isError(info)) {
+		console.log('You should use logger.error(err). Please use logger.error({err}) instead.');
+		return false;
+	}
+	return info;
+});
+
+let isError = function(e){
+	return e && e.stack && e.message;
+};
+
+const sillyWinstonConsoleFormatter = format((info) => {
+	let clone = Object.assign(info);
+	if (isError(info.message)) {
+		clone.message = `${info.message.message} ${info.message.stack}`;
+	} else if (isError(info.err)) {
+		clone.message = `${info.err.message} ${info.err.stack}`;
+	} else if (info.message && isError(info.message.err)) {
+		clone.message = `${info.message.err.message} ${info.message.err.stack}`;
+	}
+	return clone;
+});
+
+const logger = winston.createLogger({
+	level: 'silly',
+	format: warnAboutWinston(),
+	transports: [new winston.transports.Console({
+		format: format.combine(
+			sillyWinstonConsoleFormatter(),
+			winston.format.simple(),
+		),
+	})],
+});
 
 if (config.sentry.dsn) {
 	let sentryTransport = createSentryTransport(Raven);
-	transports.push(sentryTransport);
+	logger.add(sentryTransport);
 }
-
-function simpler(info) {
-	return info;
-}
-
-
-let logger = winston.createLogger({
-	format: format(simpler)(),
-	transports: transports,
-});
 
 export default logger;
