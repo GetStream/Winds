910b54663b808fc2e7d327e29620f9eca708e16d
Tommaso Barbugli <tbarbugli@gmail.com>
2018-06-04 09:22:15 +0200

    cleanup

--- api/src/utils/personalization/index.js
+++ api/src/utils/personalization/index.js
@@ -3,42 +3,34 @@ import jwt from 'jsonwebtoken';
 
 import config from '../../config';
 
-const personalization = data => {
-	return new Promise((resolve, reject) => {
-		const token = jwt.sign(
-			{
-				action: '*',
-				feed_id: '*',
-				resource: '*',
-				user_id: '*',
-			},
-			config.stream.apiSecret,
-			{ algorithm: 'HS256', noTimestamp: true },
-		);
+const personalization = async data => {
+	const token = jwt.sign(
+		{
+			action: '*',
+			feed_id: '*',
+			resource: '*',
+			user_id: '*',
+		},
+		config.stream.apiSecret,
+		{ algorithm: 'HS256', noTimestamp: true },
+	);
 
-		return axios({
-			baseURL: config.stream.baseUrl,
-			headers: {
-				'Authorization': token,
-				'Content-Type': 'application/json',
-				'Stream-Auth-Type': 'jwt',
-			},
-			method: 'GET',
-			params: {
-				api_key: config.stream.apiKey,
-				user_id: data.userId,
-			},
-			url: data.endpoint,
-		})
-			.then(res => {
-				const data = res.data.results.map(result => {
-					return result.foreign_id.split(':')[1];
-				});
-				resolve(data);
-			})
-			.catch(err => {
-				reject(err);
-			});
+	let response = await axios({
+		baseURL: config.stream.baseUrl,
+		headers: {
+			'Authorization': token,
+			'Content-Type': 'application/json',
+			'Stream-Auth-Type': 'jwt',
+		},
+		method: 'GET',
+		params: {
+			api_key: config.stream.apiKey,
+			user_id: data.userId,
+		},
+		url: data.endpoint,
+	});
+	return response.data.results.map(result => {
+		return result.foreign_id.split(':')[1];
 	});
 };
 
