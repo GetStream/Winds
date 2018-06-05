12b4e00ef48f368b698fd4c59ab642e5ea7915bc
Max Klyga <max@getstream.io>
2018-06-05 11:41:12 +0200

    Merge article and episode feed tests

--- api/test/controllers/feed.js
+++ api/test/controllers/feed.js
@@ -3,6 +3,7 @@ import { expect, request } from 'chai';
 import api from '../../src/server';
 import User from '../../src/models/user';
 import Episode from '../../src/models/episode';
+import Article from '../../src/models/article';
 import { dropDBs, getMockClient, createMockFeed, loadFixture, withLogin } from '../utils.js';
 
 describe('Feed controller', () => {
@@ -42,33 +43,29 @@ describe('Feed controller', () => {
 	describe('timeline feed', () => {
 	});
 
-	describe('article feed', () => {
-	});
+	const feeds = { 'article': Article, 'episode': Episode };
+	for (const type in feeds) {
+		describe(`${type} feed`, () => {
+			it(`should return ${type}s from user_${type} Stream feed`, async () => {
+				const content = await feeds[type].find({});
+				const userFeed = createMockFeed(`user_${type}`, user.id);
+				userFeed.get.returns(Promise.resolve({
+					results: content.map(c => {
+						return { foreign_id: `article:${c.id}` };
+					}),
+				}));
 
-	describe('episode feed', () => {
-		it('should return episodes from user_episode Stream feed', async () => {
-			const episodes = await Episode.find({});
-			const userEpisodeFeed = createMockFeed('user_episode', user.id);
-			userEpisodeFeed.get.returns(Promise.resolve({
-				results: episodes.map(e => {
-					return { foreign_id: `episode:${e.id}` };
-				}),
-			}));
+				const response = await withLogin(
+					request(api)
+						.get(`/users/${user.id}/feeds/`)
+						.query({ type, page: 1, per_page: 42 })
+				);
+				expect(response).to.have.status(200);
 
-			const response = await withLogin(
-				request(api)
-					.get(`/users/${user.id}/feeds/`)
-					.query({
-						type: 'episode',
-						page: 1,
-						per_page: 42,
-					})
-			);
-			expect(response).to.have.status(200);
-
-			const mockClient = getMockClient();
-			expect(mockClient.feed.calledWith('user_episode', user.id)).to.be.true;
-			expect(userEpisodeFeed.get.calledWith({ limit: 42, offset: 1 }));
+				const mockClient = getMockClient();
+				expect(mockClient.feed.calledWith(`user_${type}`, user.id)).to.be.true;
+				expect(userFeed.get.calledWith({ limit: 42, offset: 1 }));
+			});
 		});
-	});
+	}
 });
