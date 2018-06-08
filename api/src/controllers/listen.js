6bbaf2f63b150ff896c66561586047e6a046c874
Max Klyga <max@getstream.io>
2018-06-08 14:02:19 +0200

    Remove explicit catching of errors from controller in favor of wrapAsync. Add checks for controller method side effects and output. Silence error log when expecting errors to cleanup test runner output

--- api/src/controllers/listen.js
+++ api/src/controllers/listen.js
@@ -3,90 +3,42 @@ import User from '../models/user';
 
 import config from '../config';
 import logger from '../utils/logger';
-import events from '../utils/events';
 
 exports.list = async (req, res, _) => {
 	if (req.query.user && req.query.user != req.User.id) {
 		return res.sendStatus(403);
 	}
 
-	try {
-		const listens = await Listen.apiQuery(req.query);
-		res.json(listens);
-	} catch(err) {
-		logger.error({err});
-		res.status(422).send(err.errors);
-	}
+	const listens = await Listen.apiQuery(req.query);
+	res.json(listens);
 };
 
 exports.get = async (req, res, _) => {
-	if (req.params.listenId == 'undefined') {
+	const listen = await Listen.findOne({ _id: req.params.listenId, user: req.User.id });
+	if (!listen) {
 		return res.sendStatus(404);
 	}
 
-	try {
-		const listen = await Listen.findById(req.params.listenId);
-		if (!listen) {
-			return res.sendStatus(404);
-		}
-		if (listen.user.id != req.User.id) {
-			return res.sendStatus(403);
-		}
-
-		res.json(listen);
-	} catch(err) {
-		logger.error({err});
-		res.status(422).send(err.errors);
-	}
+	res.json(listen);
 };
 
 exports.post = async (req, res, _) => {
 	const data = Object.assign({}, req.body, { user: req.user.sub });
 
-	try {
-		const listen = await Listen.findOneAndUpdate(
-			{ user: data.user, episode: data.episode },
-			{ $set: data },
-			{ new: true, upsert: true },
-		);
-		const user = await User.findById(data.user);
-		try {
-			await events({
-				user: user._id,
-				email: user.email.toLowerCase(),
-				engagement: {
-					label: 'listen',
-					content: {
-						foreign_id: `episode:${listen.episode}`,
-						duration: listen.duration,
-					},
-				},
-			});
-			res.json(listen);
-		} catch(err) {
-			logger.error({err});
-			res.sendStatus(503);
-		}
-	} catch(err) {
-		logger.error({err});
-		res.status(422).send(err.errors);
-	}
+	const listen = await Listen.findOneAndUpdate(
+		{ user: data.user, episode: data.episode },
+		{ $set: data },
+		{ new: true, upsert: true },
+	);
+	res.json(listen);
 };
 
 exports.delete = async (req, res, _) => {
-	try {
-		const listen = await Listen.findById(req.params.listenId);
-		if (!listen) {
-			return res.sendStatus(404);
-		}
-		if (listen.user.id != req.User.id) {
-			return res.sendStatus(403);
-		}
-
-		await Listen.remove({ _id: req.params.listenId });
-		res.status(204).send();
-	} catch(err) {
-		logger.error({err});
-		res.status(422).send(err.errors);
+	const listen = await Listen.findOne({ _id: req.params.listenId, user: req.User.id });
+	if (!listen) {
+		return res.sendStatus(404);
 	}
+
+	await Listen.remove({ _id: req.params.listenId });
+	res.status(204).send();
 };
--- api/test/controllers/listen.js
+++ api/test/controllers/listen.js
@@ -1,148 +1,161 @@
+import sinon from 'sinon';
 import { expect, request } from 'chai';
 
 import User from '../../src/models/user';
 import Listen from '../../src/models/listen';
 import api from '../../src/server';
+import logger from '../../src/utils/logger';
 import { dropDBs, loadFixture, withLogin } from '../utils';
 
 describe('Listen controller', () => {
-    const episodeId = '5b0ad37026dc3db38194e286';
-    const otherUserId = '5b0f306d8e147f10f16aceb0';
-    let user;
-    let listen;
+	const episodeId = '5b0ad37026dc3db38194e286';
+	let user;
+	let listen;
 
-    before(async () => {
-        await dropDBs();
-        await loadFixture('initial-data', 'listens');
+	before(async () => {
+		await dropDBs();
+		await loadFixture('initial-data', 'listens');
 
-        user = await User.findOne();
-        listen = await Listen.findOne();
-    });
+		user = await User.findOne();
+		listen = await Listen.findOne().lean();
+	});
 
-    describe('listing all entries', () => {
-        it('should return 200 for valid request', async () => {
-            const response = await withLogin(request(api).get('/listens'));
+	describe('listing all entries', () => {
+		it('should return 200 for valid request', async () => {
+			const response = await withLogin(request(api).get('/listens'));
 
-            expect(response).to.have.status(200);
-        });
+			expect(response).to.have.status(200);
+			expect(response.body.map(l => l._id)).to.include(String(listen._id));
 
-        it('should filter results if query is provided', async () => {
-            const response = await withLogin(request(api).get('/listens').query({ episode: episodeId }));
+			const keys = ['_id', 'user', 'episode', 'duration'];
+			for (const entry of response.body) {
+				expect(Object.keys(entry)).to.include.members(keys);
+			}
+		});
+		
+		it('should filter results if query is provided', async () => {
+			const response = await withLogin(request(api).get('/listens').query({ episode: episodeId }));
 
-            expect(response).to.have.status(200);
-        });
+			expect(response).to.have.status(200);
+			expect(response.body.map(l => l._id)).to.include(String(listen._id));
+		});
 
-        it('should return 403 when retrieving entries created by other users', async () => {
-            const response = await withLogin(request(api).get('/listens').query({ user: otherUserId }));
+		it('should return 500 for invalid request', async () => {
+			//XXX: silensing error logs to have nice clean test output
+			sinon.stub(logger, 'error');
+			const response = await withLogin(request(api).get('/listens').query({ episode: { $gte: '' } }));
 
-            expect(response).to.have.status(403);
-        });
+			expect(response).to.have.status(500);
+			logger.error.restore();
+		});
+	});
 
-        it('should return 422 for invalid request', async () => {
-            const response = await withLogin(request(api).get('/listens').query({ episode: { $gte: '' } }));
+	describe('retrieving entry by id', () => {
+		it('should return 200 for valid request', async () => {
+			const response = await withLogin(request(api).get(`/listens/${listen._id}`));
 
-            expect(response).to.have.status(422);
-        });
-    });
+			expect(response).to.have.status(200);
+			const keys = ['_id', 'user', 'episode', 'duration'];
+			expect(Object.keys(response.body)).to.include.members(keys);
 
-    describe('retrieving entry by id', () => {
-        it('should return 200 for valid request', async () => {
-            const response = await withLogin(request(api).get(`/listens/${listen._id}`));
+			for (const key of keys) {
+				//XXX: converting to string to avoid type differences
+				expect(String(response.body[key]._id || response.body[key])).to.be.equal(String(listen[key]));
+			}
+		});
 
-            expect(response).to.have.status(200);
-        });
+		it('should return 404 for invalid id', async () => {
+			const response = await withLogin(request(api).get('/listens/5b0f306d8e147f10deadbeef'));
 
-        it('should return 404 for invalid id', async () => {
-            const response = await withLogin(request(api).get('/listens/5b0f306d8e147f10deadbeef'));
+			expect(response).to.have.status(404);
+		});
 
-            expect(response).to.have.status(404);
-        });
+		it('should return 500 for invalid request', async () => {
+			//XXX: silensing error logs to have nice clean test output
+			sinon.stub(logger, 'error');
+			const response = await withLogin(request(api).get('/listens/<bogus-id>/'));
 
-        it('should return 403 when retrieving entries created by other users', async () => {
-            const otherListen = await Listen.create({
-                user: otherUserId,
-                episode: episodeId
-            });
-            const response = await withLogin(request(api).get(`/listens/${otherListen._id}`));
+			expect(response).to.have.status(500);
+			logger.error.restore();
+		});
+	});
 
-            expect(response).to.have.status(403);
-        });
+	describe('upsert entry', () => {
+		it('should return 200 for new entry', async () => {
+			const data = {
+				user: user._id,
+				episode: episodeId,
+				duration: 33
+			};
+			const response = await withLogin(request(api).post('/listens').send(data));
 
-        it('should return 422 for invalid request', async () => {
-            const response = await withLogin(request(api).get('/listens/<bogus-id>/'));
+			expect(response).to.have.status(200);
+			const keys = ['user', 'episode', 'duration'];
+			expect(Object.keys(response.body)).to.include.members(keys);
 
-            expect(response).to.have.status(422);
-        });
-    });
+			for (const key of keys) {
+				//XXX: converting to string to avoid type differences
+				expect(String(response.body[key])).to.be.equal(String(data[key]));
+			}
+		});
 
-    describe('upsert entry', () => {
-        it('should return 200 for new entry', async () => {
-            const response = await withLogin(
-                request(api)
-                    .post('/listens')
-                    .send({
-                        user: user._id,
-                        episode: episodeId,
-                        duration: 0
-                    })
-            );
+		it('should return 200 for existing entry', async () => {
+			const data = {
+				_id: listen._id,
+				user: user._id,
+				episode: episodeId,
+				duration: 66
+			};
+			const response = await withLogin(request(api).post('/listens').send(data));
 
-            expect(response).to.have.status(200);
-        });
+			expect(response).to.have.status(200);
 
-        it('should return 200 for existing entry', async () => {
-            const response = await withLogin(
-                request(api)
-                    .post('/listens')
-                    .send({
-                        _id: listen._id,
-                        user: user._id,
-                        episode: episodeId,
-                        duration: 10
-                    })
-            );
+			const updatedListen = await Listen.findById(listen._id).lean();
+			const keys = ['_id', 'user', 'episode', 'duration'];
+			expect(Object.keys(response.body)).to.include.members(keys);
 
-            expect(response).to.have.status(200);
-        });
+			for (const key of keys) {
+				//XXX: converting to string to avoid type differences
+				expect(String(response.body[key])).to.be.equal(String(data[key]));
+				expect(String(updatedListen[key])).to.be.equal(String(data[key]));
+			}
+		});
 
-        it('should return 422 for invalid request', async () => {
-            const response = await withLogin(
-                request(api)
-                    .post('/listens')
-                    .send({ episode: { $gte: { and: 1 } } })
-            );
+		it('should return 500 for invalid request', async () => {
+			//XXX: silensing error logs to have nice clean test output
+			sinon.stub(logger, 'error');
+			const response = await withLogin(
+				request(api)
+					.post('/listens')
+					.send({ episode: { $gte: { and: 1 } } })
+			);
 
-            expect(response).to.have.status(422);
-        });
-    });
+			expect(response).to.have.status(500);
+			logger.error.restore();
+		});
+	});
 
-    describe('deleting entry by id', () => {
-        it('should return 204 for valid request', async () => {
-            const response = await withLogin(request(api).delete(`/listens/${listen._id}`));
+	describe('deleting entry by id', () => {
+		it('should return 204 for valid request', async () => {
+			const response = await withLogin(request(api).delete(`/listens/${listen._id}`));
 
-            expect(response).to.have.status(204);
-        });
+			expect(response).to.have.status(204);
+			expect(await Listen.findById(listen._id)).to.be.null;
+		});
 
-        it('shoudl return 404 for invalid id', async () => {
-            const response = await withLogin(request(api).delete('/listens/5b0f306d8e147f10deadbeef'));
+		it('shoudl return 404 for invalid id', async () => {
+			const response = await withLogin(request(api).delete('/listens/5b0f306d8e147f10deadbeef'));
 
-            expect(response).to.have.status(404);
-        });
+			expect(response).to.have.status(404);
+		});
 
-        it('should return 403 when deleting entries created by other users', async () => {
-            const otherListen = await Listen.create({
-                user: otherUserId,
-                episode: episodeId
-            });
-            const response = await withLogin(request(api).delete(`/listens/${otherListen._id}`));
+		it('should return 500 for invalid request', async () => {
+			//XXX: silensing error logs to have nice clean test output
+			sinon.stub(logger, 'error');
+			const response = await withLogin(request(api).delete('/listens/<bogus-id>/'));
 
-            expect(response).to.have.status(403);
-        });
-
-        it('should return 422 for invalid request', async () => {
-            const response = await withLogin(request(api).delete('/listens/<bogus-id>/'));
-
-            expect(response).to.have.status(422);
-        });
-    });
+			expect(response).to.have.status(500);
+			logger.error.restore();
+		});
+	});
 });
