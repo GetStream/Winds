44feb83d28e37b1ec7747573fcc5c8651c63d7db
Thierry Schellenbach <thierryschellenbach@gmail.com>
2018-06-16 13:55:30 -0600

    better upsert behaviour

--- api/src/utils/upsert.js
+++ api/src/utils/upsert.js
@@ -13,106 +13,122 @@ import logger from '../utils/logger';
 
 import { getStatsDClient, timeIt } from '../utils/statsd';
 
-const streamClient = stream.connect(config.stream.apiKey, config.stream.apiSecret);
+const streamClient = stream.connect(
+	config.stream.apiKey,
+	config.stream.apiSecret,
+);
 const duplicateKeyError = 11000;
-const immutableFields = ['publicationDate', 'createdAt', 'updatedAt', 'id', '_id']
+const immutableFields = ['publicationDate', 'createdAt', 'updatedAt', 'id', '_id'];
 
 const statsd = getStatsDClient();
 
-
 // upsertManyPosts at once at super speed
 export async function upsertManyPosts(publicationID, newPosts, publicationType) {
-  let schema = (publicationType == 'rss') ? Article : Episode
-  let schemaField = publicationType
+	let schema = publicationType == 'rss' ? Article : Episode;
+	let schemaField = publicationType;
 
-  // step 1: get the existing objects in mongodb
-  let fingerprints = newPosts.map(p=>p.fingerprint)
-  let lookup = {fingerprint: {$in: fingerprints}}
-  lookup[schemaField] = publicationID
-  let existingPosts = await schema.find().lean()
-  let existingPostsMap = {}
-  for (let p of existingPosts) {
-    existingPostsMap[p.fingerprint] = p
-  }
+	// step 1: get the existing objects in mongodb
+	let fingerprints = newPosts.map(p => p.fingerprint);
+	let lookup = { fingerprint: { $in: fingerprints } };
+	lookup[schemaField] = publicationID;
+	let existingPosts = await schema.find().lean();
+	let existingPostsMap = {};
+	for (let p of existingPosts) {
+		existingPostsMap[p.fingerprint] = p;
+	}
 
-  // step 2: make a list of posts we should update
-  let operationMap = {new: [], changed: [], unchanged: []}
-  let operations = []
-  for (let p of newPosts) {
-    if (!p[schemaField]) {
-      throw new Error(`You forgot to specify the ${schemaField} field`)
+	// step 2: make a list of posts we should update
+	let operationMap = { new: [], changed: [], unchanged: [] };
+	let operations = [];
+	for (let p of newPosts) {
+		if (!p[schemaField]) {
+			throw new Error(`You forgot to specify the ${schemaField} field`);
+		}
+		let postData = p.toObject ? p.toObject() : p;
+		if (p.fingerprint in existingPostsMap) {
+			let existing = existingPostsMap[p.fingerprint];
+			if (postChanged(existing, p)) {
+				// make sure we don't use the generated mongodb id that hasn't been saved yet
+				// we want the new data, with the old id
+				p._id = existing._id;
+				operationMap.changed.push(p);
+				// filter on both rss and fingerprint so we can use the index
+				const filter = { fingerprint: existing.fingerprint };
+				filter[schemaField] = publicationID;
+        delete postData['_id'];
+				operations.push({
+					updateOne: {
+						filter: filter,
+						update: postData,
+					},
+				});
+			} else {
+				operationMap.unchanged.push(p);
+			}
+		} else {
+			// insert scenario
+			operationMap.new.push(p);
+
+			operations.push({
+				insertOne: {
+					document: postData,
+				},
+			});
+		}
+	}
+
+	// step 3: apply the update via bulkWrite
+	// https://docs.mongodb.com/manual/core/bulk-write-operations/
+	// http://mongoosejs.com/docs/api.html#bulkwrite_bulkWrite
+	if (operations.length) {
+		try {
+			let response = await schema.bulkWrite(operations, { ordered: false });
+		} catch (e) {
+      // since we use an unordered query it doesnt matter if we hit a few unique constraints
+			if (e.code != duplicateKeyError) {
+				throw e;
+			}
+		}
+	}
+
+	// step 4: statsd tracking on new, updated and unchanged
+	// TODO: Tommaso! Do you agree with the format?
+	for (const [k, v] of Object.entries(operationMap)) {
+		statsd.increment(`winds.handle_rss.${schemaField}.${k}`, v.length);
+	}
+
+  // sanity check
+  let updated = operationMap.new.concat(operationMap.changed)
+  for (let u of updated) {
+    if (!u._id) {
+      throw Error(`missing id for object ${u.fingerprint} and feed ${publicationID}`)
     }
-    let postData = (p.toObject) ? p.toObject() : p
-    delete postData['_id']
-    if (p.fingerprint in existingPostsMap) {
-      let existing = existingPostsMap[p.fingerprint]
-      if (postChanged(existing, p)) {
-        // make sure we don't use the generated mongodb id that hasn't been saved yet
-        // we want the new data, with the old id
-        p._id = existing._id
-        operationMap.changed.push(p)
-        // filter on both rss and fingerprint so we can use the index
-        const filter = {fingerprint: existing.fingerprint}
-        filter[schemaField] = publicationID
-        operations.push({
-          updateOne: {
-            filter: filter,
-            update: postData
-          }
-        })
-      } else {
-        operationMap.unchanged.push(p)
-      }
-    } else {
-      // insert scenario
-      operationMap.new.push(p)
-
-      operations.push({
-        insertOne: {
-          document: postData
-        }
-      })
-    }
   }
 
-  // step 3: apply the update via bulkWrite
-  // https://docs.mongodb.com/manual/core/bulk-write-operations/
-  // http://mongoosejs.com/docs/api.html#bulkwrite_bulkWrite
-  if (operations.length) {
-    let response = await schema.bulkWrite(operations, {ordered: false})
-    // TODO: How to handle errors, such as duplicate keys
-  }
-
-  // step 4: statsd tracking on new, updated and unchanged
-  // TODO: Tommaso! Do you agree with the format?
-  for (const [k,v] of Object.entries(operationMap)) {
-    statsd.increment(`winds.handle_rss.${schemaField}.${k}`, v.length);
-  }
-
-  return operationMap
+	return operationMap;
 }
 
 export function normalizePost(post) {
-  let postObject = (post.toObject) ? post.toObject() : post
-  // these ids are not present in the RSS feed, so that causes issues
-  for (let e of postObject.enclosures) {
-    delete e['_id']
-  }
-  return postObject
+	let postObject = post.toObject ? post.toObject() : post;
+	// these ids are not present in the RSS feed, so that causes issues
+	for (let e of postObject.enclosures) {
+		delete e['_id'];
+	}
+	return postObject;
 }
 
 // compare 2 posts and see if they changed
 export function postChanged(existingPost, newPost) {
-  let existingObject = normalizePost(existingPost)
-  let newObject = normalizePost(newPost)
-  // handle the fact that images are updated via OG scraping, so we only care if more became available
-  newObject.images = Object.assign(existingObject.images, newObject.images);
-  let objectDiff = diff(existingObject, newObject)
-  // remove the immutable fields from the diff
-  for (let f of immutableFields) {
-    delete objectDiff[f]
-  }
-  let changes = Object.keys(objectDiff).length
+	let existingObject = normalizePost(existingPost);
+	let newObject = normalizePost(newPost);
+	// handle the fact that images are updated via OG scraping, so we only care if more became available
+	newObject.images = Object.assign(existingObject.images, newObject.images);
+	let objectDiff = diff(existingObject, newObject);
+	// remove the immutable fields from the diff
+	for (let f of immutableFields) {
+		delete objectDiff[f];
+	}
+	let changes = Object.keys(objectDiff).length;
 
-  return changes
+	return changes;
 }
--- api/test/utilities/upsert.js
+++ api/test/utilities/upsert.js
@@ -10,6 +10,7 @@ import Episode from '../../src/models/episode';
 import mongoose from 'mongoose';
 
 import logger from '../../src/utils/logger';
+const duplicateKeyError = 11000;
 
 import { loadFixture, getMockClient, getMockFeed, dropDBs } from '../utils';
 import { postChanged, upsertManyPosts } from '../../src/utils/upsert';
@@ -38,87 +39,161 @@ describe('Upsert', () => {
 		it('the same article shouldnt trigger an update', async () => {
 			let operationMap = await upsertManyPosts(article1.rss, [article1], 'rss');
 			expect(operationMap.changed.length).to.equal(0);
-      expect(operationMap.new.length).to.equal(0);
+			expect(operationMap.new.length).to.equal(0);
 		});
 		it('url change should result in an update', async () => {
-			article1.url = 'https://google.com/test';
-			let operationMap = await upsertManyPosts(article1.rss, [article1], 'rss');
+			let a = Object.assign({}, article1)
+			a.url = 'https://google.com/test'
+			let article3 = new Article(a);
+
+			let operationMap = await upsertManyPosts(article1.rss, [article3], 'rss');
 			expect(operationMap.changed.length).to.equal(1);
-      expect(operationMap.new.length).to.equal(0);
+			expect(operationMap.new.length).to.equal(0);
+			expect(await Article.findOne({_id: operationMap.changed[0]._id})).to.not.be.null
+
 		});
-		it('publicationDate change shouldnt result in an update', async () => {
-			article1.publicationDate = new Date();
-			let operationMap = await upsertManyPosts(article1.rss, [article1], 'rss');
+		it.skip('publicationDate change shouldnt result in an update', async () => {
+			let a = Object.assign({}, article1)
+			a.publicationDate = new Date();
+			let article3 = new Article(a);
+			let operationMap = await upsertManyPosts(article1.rss, [article3], 'rss');
 			expect(operationMap.changed.length).to.equal(0);
-      expect(operationMap.new.length).to.equal(0);
+			expect(operationMap.new.length).to.equal(0);
 		});
 		it('a new article should be inserted', async () => {
-      let article3 = article1
-			article3.fingerprint = 'hello world';
-			article3['_id'] = null
-			article3.url = 'https://google.com/testhelloworld';
+			let article3 = new Article({fingerprint: "hello world", url: "https://google.com/testhelloworld", rss: article1.rss, title: "hi"});
 			let operationMap = await upsertManyPosts(article1.rss, [article3], 'rss');
 			expect(operationMap.new.length).to.equal(1);
+			expect(await Article.findOne({_id: operationMap.new[0]._id})).to.not.be.null
 		});
 	});
 
 	describe('Episode upsertManyPosts', () => {
 		it('the same episode shouldnt trigger an update', async () => {
-			let operationMap = await upsertManyPosts(episode1.podcast, [episode1], 'podcast');
+			let operationMap = await upsertManyPosts(
+				episode1.podcast,
+				[episode1],
+				'podcast',
+			);
 			expect(operationMap.changed.length).to.equal(0);
-      expect(operationMap.new.length).to.equal(0);
+			expect(operationMap.new.length).to.equal(0);
 		});
 		it('a new episode should be inserted', async () => {
-      let episode3 = episode1
-			episode3.fingerprint = 'hello world';
-			episode3['_id'] = null
-			episode3.url = 'https://google.com/testhelloworld';
-			let operationMap = await upsertManyPosts(episode1.podcast, [episode1], 'podcast');
+			let episode3 = new Episode({fingerprint: "hello world 2", url: "hi", title: "hi2", podcast: episode1.podcast});
+			let operationMap = await upsertManyPosts(
+				episode1.podcast,
+				[episode3],
+				'podcast',
+			);
 			expect(operationMap.new.length).to.equal(1);
 		});
-	})
+	});
 
 	describe('Double Inserts RSS feed', () => {
-
 		// loop over feeds, and verify that the 2nd insert has 0 changes
 		// this will break if something is wrong with the upsert/change detection
-		for (let f of ['techcrunch', 'reddit-r-programming', 'hackernews', 'medium-technology']) {
+		for (let f of [
+			'techcrunch',
+			'reddit-r-programming',
+			'hackernews',
+			'medium-technology',
+		]) {
 			it(`the second run should be unchanged for ${f}`, async () => {
 				let posts = await ReadFeedStream(getTestFeed(f));
 				let feedResponse = ParseFeedPosts(posts);
-				let articles = feedResponse.articles
+				let articles = feedResponse.articles;
 
 				for (let a of articles) {
-					a['rss'] = '5b0ad37226dc3db38194e5ef'
+					a['rss'] = '5b0ad37226dc3db38194e5ef';
 				}
 				await upsertManyPosts('5b0ad37226dc3db38194e5ef', articles, 'rss');
-				let operationMap = await upsertManyPosts('5b0ad37226dc3db38194e5ef', articles, 'rss');
-				expect(operationMap.new.length).to.equal(0)
-				expect(operationMap.changed.length).to.equal(0)
+				let operationMap = await upsertManyPosts(
+					'5b0ad37226dc3db38194e5ef',
+					articles,
+					'rss',
+				);
+				expect(operationMap.new.length).to.equal(0);
+				expect(operationMap.changed.length).to.equal(0);
 			});
 		}
-	})
+	});
+
+	describe('BulkWrite behaviour', () => {
+		// article ids are generated upon object creation
+		// they remain the same during insert
+		// bulk write error handling
+
+		it(`article instantiation`, async () => {
+			let a = new Article({ fingerprint: 'hihi', url: 'https://google.com/' });
+			expect(a.id).to.not.be.null;
+		});
+
+		it(`article create`, async () => {
+			let a = new Article({
+				fingerprint: 'hihi2',
+				url: 'https://google.com/',
+				rss: '5b0ad37226dc3db38194e5ef',
+				title: 123,
+			});
+			let data = a.toObject();
+			let a2 = await Article.create(data);
+			expect(a.id).to.equal(a2.id);
+		});
+
+		it(`bulkwrite errors`, async () => {
+			let a = new Article({
+				fingerprint: 'hihi3',
+				url: 'https://google.com/',
+				rss: '5b0ad37226dc3db38194e5ef',
+				title: 123,
+			});
+			let data = a.toObject();
+			delete data['_id'];
+			let operations = [];
+			operations.push({
+				insertOne: {
+					document: data,
+				},
+			});
+			operations.push({
+				insertOne: {
+					document: data,
+				},
+			});
+			// should fail since the fingerprint and rss is the same
+			try {
+				let results = await Article.bulkWrite(operations, { ordered: false });
+			} catch (e) {
+				if (e.code != duplicateKeyError) {
+					throw e;
+				}
+			}
+		});
+	});
 
 	describe('Double Inserts Podcasts', () => {
-
 		// loop over feeds, and verify that the 2nd insert has 0 changes
 		// this will break if something is wrong with the upsert/change detection
 		for (let f of ['giant-bombcast', 'serial', 'a16z']) {
 			it(`the second run should be unchanged for ${f}`, async () => {
 				let posts = await ReadFeedStream(getTestPodcast(f));
 				let feedResponse = ParsePodcastPosts(posts);
-				let episodes = feedResponse.episodes
+				let episodes = feedResponse.episodes;
 
 				for (let e of episodes) {
-					e['podcast'] = '5b0ad37226dc3db38194e5ef'
+					e['podcast'] = '5b0ad37226dc3db38194e5ef';
 				}
 				await upsertManyPosts('5b0ad37226dc3db38194e5ef', episodes, 'podcast');
-				let operationMap = await upsertManyPosts('5b0ad37226dc3db38194e5ef', episodes, 'podcast');
-				expect(operationMap.new.length).to.equal(0)
-				expect(operationMap.changed.length).to.equal(0)
+				let operationMap = await upsertManyPosts(
+					'5b0ad37226dc3db38194e5ef',
+					episodes,
+					'podcast',
+				);
+				expect(operationMap.new.length).to.equal(0);
+				expect(operationMap.changed.length).to.equal(0);
 			});
 		}
-	})
+	});
 
 	describe('postChanged diff function', () => {
 		it('the diff between these 2 articles should be 4', async () => {
