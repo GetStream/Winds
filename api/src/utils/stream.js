a8ac991cd729d24e32b5ee4339337997aacba064
Thierry Schellenbach <thierryschellenbach@gmail.com>
2018-06-19 16:31:39 -0600

    connect to stream only once

---
+++ api/src/utils/stream.js
@@ -0,0 +1,11 @@
+import stream from 'getstream';
+import config from '../config';
+
+var streamClient = null
+
+export function getStreamClient() {
+  if (streamClient === null) {
+    streamClient = stream.connect(config.stream.apiKey, config.stream.apiSecret);
+  }
+  return streamClient
+}
