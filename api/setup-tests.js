d3550ea6bb8818fa0b4894c2f70b6a84f1481571
Max Klyga <max@getstream.io>
2018-06-19 14:50:52 +0200

    Give travis-ci more time before shut-down

--- api/setup-tests.js
+++ api/setup-tests.js
@@ -69,6 +69,9 @@ wrapMocha(async () => {
 		}
 	});
 }, failures => {
+	//XXX: it seems Travis-ci is having trouble with process wrap-up procedures so lets
+	//     allocate more time before shutting down
+	const timeout = process.env.TRAVIS ? 10000 : 1500;
 	//XXX: don't care about open connections
 	setTimeout(() => process.exit(failures ? 1 : 0), 1500);
 });
