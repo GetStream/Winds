4449e2879898a3097407cdac558ff8f16cde5e8d
Nick Parsons <nparsons08@gmail.com>
2018-06-19 15:48:25 -0600

    Tests for updated POST /pins endpoint

--- api/test/controllers/pins.js
+++ api/test/controllers/pins.js
@@ -6,7 +6,6 @@ import Article from '../../src/models/article';
 import Episode from '../../src/models/episode';
 import { loadFixture, withLogin, dropDBs } from '../utils';
 
-
 describe('Pin controller', () => {
 	let pin;
 	let article;
@@ -23,18 +22,14 @@ describe('Pin controller', () => {
 
 	describe('get', () => {
 		it('should return all pins', async () => {
-			const res = await withLogin(
-				request(api).get('/pins')
-			);
+			const res = await withLogin(request(api).get('/pins'));
 			expect(res).to.have.status(200);
 		});
 	});
 
 	describe('get', () => {
 		it('should return the a single pin via /pins/:pinId', async () => {
-			const res = await withLogin(
-				request(api).get(`/pins/${pin._id}`)
-			);
+			const res = await withLogin(request(api).get(`/pins/${pin._id}`));
 			expect(res).to.have.status(200);
 		});
 	});
@@ -42,7 +37,9 @@ describe('Pin controller', () => {
 	describe('get', () => {
 		it('should return a limited number of pins', async () => {
 			const res = await withLogin(
-				request(api).get('/pins').query({ limit: 1 })
+				request(api)
+					.get('/pins')
+					.query({ limit: 1 }),
 			);
 			expect(res).to.have.status(200);
 			expect(res.body).to.be.an('array');
@@ -50,28 +47,32 @@ describe('Pin controller', () => {
 	});
 
 	describe('post', () => {
-		it('should create an article pin', async () => {
+		it('should create an article pin and return the hydrated article', async () => {
 			const res = await withLogin(
-				request(api).post('/pins').send({ article: article._id })
+				request(api)
+					.post('/pins')
+					.send({ article: article._id }),
 			);
 			expect(res).to.have.status(200);
+			expect(res.body).to.have.property('article');
 		});
 	});
 
 	describe('post', () => {
-		it('should create an episode pin', async () => {
+		it('should create an episode pin and return a hydrated episode', async () => {
 			const res = await withLogin(
-				request(api).post('/pins').send({ episode: episode._id })
+				request(api)
+					.post('/pins')
+					.send({ episode: episode._id }),
 			);
 			expect(res).to.have.status(200);
+			expect(res.body).to.have.property('episode');
 		});
 	});
 
 	describe('delete', () => {
 		it('should delete a pin', async () => {
-			const res = await withLogin(
-				request(api).delete(`/pins/${pin._id}`)
-			);
+			const res = await withLogin(request(api).delete(`/pins/${pin._id}`));
 			expect(res).to.have.status(204);
 		});
 	});
