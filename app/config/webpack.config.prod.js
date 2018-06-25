4bad3847618da6ce75c527b65569999a39ed82ec
Ken Hoff <ken@hoff.tech>
2018-06-25 14:29:00 -0600

    removing minification

--- app/config/webpack.config.prod.js
+++ app/config/webpack.config.prod.js
@@ -258,27 +258,6 @@ module.exports = {
 		// It is absolutely essential that NODE_ENV was set to production here.
 		// Otherwise React will be compiled in the very slow development mode.
 		new webpack.DefinePlugin(env.stringified),
-		// Minify the code.
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-				// Disabled because of an issue with Uglify breaking seemingly valid code:
-				// https://github.com/facebookincubator/create-react-app/issues/2376
-				// Pending further investigation:
-				// https://github.com/mishoo/UglifyJS2/issues/2011
-				comparisons: false,
-			},
-			mangle: {
-				safari10: true,
-			},
-			output: {
-				comments: false,
-				// Turned on because emoji and regex is not minified properly using default
-				// https://github.com/facebookincubator/create-react-app/issues/2488
-				ascii_only: true,
-			},
-			sourceMap: shouldUseSourceMap,
-		}),
 		// Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
 		new ExtractTextPlugin({
 			filename: cssFilename,
