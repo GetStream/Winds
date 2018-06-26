398ddd3fcfc4c2dbc70acc8a6c8ce5ce966bcf75
Thierry Schellenbach <thierryschellenbach@gmail.com>
2018-06-26 11:50:45 -0600

    wip

--- api/src/utils/personalization/index.js
+++ api/src/utils/personalization/index.js
@@ -5,79 +5,57 @@ import Podcast from '../../models/podcast';
 import RSS from '../../models/rss';
 import Episode from '../../models/episode';
 import logger from '../../utils/logger';
+import { getStreamClient } from '../../utils/stream';
 
 import config from '../../config';
-
-export function createGlobalToken() {
-	const token = jwt.sign(
-		{
-			action: '*',
-			feed_id: '*',
-			resource: '*',
-			user_id: '*',
-		},
-		config.stream.apiSecret,
-		{ algorithm: 'HS256', noTimestamp: true },
-	);
-	return token;
-}
 
 export async function getRecommendations(userID, type, limit) {
 	if (!userID) {
 		throw Error('missing user id');
 	}
-	const token = createGlobalToken();
-	const path = `/winds_${type}_recommendations`;
-	const params = {
-		api_key: config.stream.apiKey,
+	const streamClient = getStreamClient();
+	const path = `winds_${type}_recommendations`;
+	const queryParams = {
 		user_id: userID,
 		limit: limit,
+	};
+	let response;
+	try {
+		response = await streamClient.personalization.get(path, queryParams);
+	} catch (e) {
+		console.log('e', e);
 	}
-
-	let response = await axios({
-		baseURL: config.stream.baseUrl,
-		headers: {
-			Authorization: token,
-			'Content-Type': 'application/json',
-			'Stream-Auth-Type': 'jwt',
-		},
-		method: 'GET',
-		params: params,
-		url: path,
-	});
 	let objectIDs = response.data.results.map(result => {
 		return result.foreign_id.split(':')[1];
 	});
 	return objectIDs;
 }
 
-export async function getRSSRecommendations(userID, limit=20) {
+export async function getRSSRecommendations(userID, limit = 20) {
 	let ids = await getRecommendations(userID, 'rss', limit);
 	return RSS.find({ _id: { $in: ids } });
 }
 
-
-
-export async function getPodcastRecommendations(userID, limit=20) {
+export async function getPodcastRecommendations(userID, limit = 20) {
 	let ids = await getRecommendations(userID, 'podcast', limit);
 
 	return Podcast.find({ _id: { $in: ids } });
 }
 
-export async function getEpisodeRecommendations(userID, limit=20) {
+export async function getEpisodeRecommendations(userID, limit = 20) {
 	let ids = await getRecommendations(userID, 'episode', limit);
 	let episodes = await Episode.find({ _id: { $in: ids } });
 	if (ids.length != episodes.length) {
-		logger.warn(`failed to find some episodes from list ${ids.join(',')}`)
+		logger.warn(`failed to find some episodes from list ${ids.join(',')}`);
 	}
-	return episodes
+	return episodes;
 }
 
-export async function getArticleRecommendations(userID, limit=20) {
+export async function getArticleRecommendations(userID, limit = 20) {
 	let ids = await getRecommendations(userID, 'article', limit);
 	let articles = await Article.find({ _id: { $in: ids } });
 	if (ids.length != articles.length) {
-		logger.warn(`failed to find some articles from list ${ids.join(',')}`)
+		logger.warn(`failed to find some articles from list ${ids.join(',')}`);
 	}
-	return articles
+	return articles;
 }
