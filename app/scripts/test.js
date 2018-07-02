7ae0b0fce2eba043d450abbe44402fced8a61ed9
Nick Parsons <nparsons08@gmail.com>
2018-07-02 09:23:47 -0600

    Prettified scripts

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
@@ -9,7 +9,7 @@ process.env.PUBLIC_URL = '';
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
-  throw err;
+	throw err;
 });
 
 // Ensure environment variables are read.
@@ -20,8 +20,7 @@ let argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
+	argv.push('--watch');
 }
-
 
 jest.run(argv);
