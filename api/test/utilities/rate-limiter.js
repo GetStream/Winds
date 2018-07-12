09ddb883c782667fe86bfab208d92ba26b15ce76
Max Klyga <max@getstream.io>
2018-07-12 16:17:14 +0200

    Tune rate limiter ticks to ensure we don't wait long enough so that 3rd party code triggers setTimeout by accident

--- api/test/utilities/rate-limiter.js
+++ api/test/utilities/rate-limiter.js
@@ -16,31 +16,31 @@ describe('Rate limiter', () => {
     });
 
     it('shouldn\'t block until max capacity is reached', async () => {
-        const limiter = rateLimiter(600);
-        for (let i = 0; i < 600; ++i) {
+        const limiter = rateLimiter(5);
+        for (let i = 0; i < 5; ++i) {
             await limiter.tick();
         }
         expect(setTimeout.called).to.be.false;
     });
 
     it('should block when over max capacity', async () => {
-        const limiter = rateLimiter(600);
-        for (let i = 0; i < 600; ++i) {
+        const limiter = rateLimiter(1000);
+        for (let i = 0; i < 1000; ++i) {
             await limiter.tick();
         }
         const before = Date.now();
         await limiter.tick();
         const after = Date.now();
         expect(setTimeout.calledOnce).to.be.true;
-        expect(after - before).to.be.closeTo(100, 5);
+        expect(after - before).to.be.closeTo(60, 5);
     });
 
     it('shouldn\'t block after waiting', async () => {
-        const limiter = rateLimiter(600);
-        for (let i = 0; i < 600; ++i) {
+        const limiter = rateLimiter(1000);
+        for (let i = 0; i < 1000; ++i) {
             await limiter.tick();
         }
-        await new Promise((resolve, _) => originalSetTimeout(resolve, 100));
+        await new Promise((resolve, _) => originalSetTimeout(resolve, 60));
         await limiter.tick();
         expect(setTimeout.called).to.be.false;
     });
