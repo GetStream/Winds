06f0580d4657aab40b2f118de04f705e248822c1
Max Klyga <necroment@gmail.com>
2018-08-02 14:32:59 +0200

    Feature/rate limit opml follows (#463)
    * Add a rate limit of  3k opml feed imports per user per day
    * Use legacy URL parsing API to be compatible with older Node versions
    * Refactor OPML feed following to batch operations

---
+++ api/src/utils/rate-limiter.js
@@ -0,0 +1,54 @@
+import Redis from 'ioredis';
+
+import config from '../config';
+
+const redis = new Redis(config.cache.uri);
+
+redis.defineCommand('rateLimit', {
+	numberOfKeys: 2,
+	lua: `
+		local requestsPerDay = tonumber(ARGV[1])
+		local microsecondsPerDay = 24 * 60 * 60 * 1000 * 1000
+		local requestsPerMicrosecond = requestsPerDay / microsecondsPerDay
+		local microsecondsPerRequest = 1 / requestsPerMicrosecond
+
+		local time = redis.call('TIME')
+		local now = tonumber(time[1]) * 1000000 + tonumber(time[2])
+
+		local value = tonumber(redis.call('GET', KEYS[1])) or requestsPerDay
+		local lastUpdate = tonumber(redis.call('GET', KEYS[2])) or now
+
+		local timePassed = now - lastUpdate
+		local newValue = math.min(requestsPerDay, value + requestsPerMicrosecond * timePassed)
+		local timeUntilRefil = math.min(microsecondsPerRequest, math.max(0, microsecondsPerRequest - timePassed))
+		redis.replicate_commands()
+		if math.floor(newValue) > 0 then
+			newValue = newValue - 1
+			redis.call('SET', KEYS[2], now)
+			timeUntilRefil = -1
+		end
+		redis.call('SET', KEYS[1], newValue)
+		return tostring(timeUntilRefil)
+	`
+});
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+export async function tick(userID, requestsPerDay=3000) {
+	const valueKey = `rate-limit:${userID}:value`;
+	const lastUpdateKey = `rate-limit:${userID}:last-update`;
+	const timeUntilRefil = parseFloat(await redis.rateLimit(valueKey, lastUpdateKey, requestsPerDay));
+	if (timeUntilRefil === -1) {
+		return;
+	}
+	await sleep(timeUntilRefil / 1000);
+	return tick(userID);
+}
+
+export async function reset(userID) {
+	const valueKey = `rate-limit:${userID}:value`;
+	const lastUpdateKey = `rate-limit:${userID}:last-update`;
+    return await redis.del(valueKey, lastUpdateKey);
+}
---
+++ api/test/utilities/rate-limiter.js
@@ -0,0 +1,46 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+import * as limiter from '../../src/utils/rate-limiter';
+
+describe('Rate limiter', () => {
+    let originalSetTimeout;
+
+    beforeEach(async () => {
+        await limiter.reset('fake-id');
+        originalSetTimeout = global.setTimeout;
+        global.setTimeout = sinon.spy(originalSetTimeout);
+    });
+
+    afterEach(() => {
+        global.setTimeout = originalSetTimeout;
+    });
+
+    it('shouldn\'t block until max capacity is reached', async () => {
+        for (let i = 0; i < 3000; ++i) {
+            await limiter.tick('fake-id');
+        }
+        expect(setTimeout.called).to.be.false;
+    });
+
+    it('should block when over max capacity', async () => {
+        for (let i = 0; i < 3000; ++i) {
+            await limiter.tick('fake-id');
+        }
+        const before = Date.now();
+        await limiter.tick('fake-id');
+        const after = Date.now();
+        expect(after - before).to.be.closeTo(28800, 50);
+    }).timeout(45000);
+
+    it('shouldn\'t block after waiting', async () => {
+        for (let i = 0; i < 3000; ++i) {
+            await limiter.tick('fake-id');
+        }
+        await new Promise(resolve => originalSetTimeout(resolve, 28800));
+        const before = Date.now();
+        await limiter.tick('fake-id');
+        const after = Date.now();
+        expect(after - before).to.be.closeTo(0, 20);
+    }).timeout(45000);
+});
