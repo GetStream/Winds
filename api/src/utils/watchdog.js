fe2ad7e4a1c4e163385c9d6788396d260cae357a
Max Klyga <max@getstream.io>
2018-08-17 17:37:16 +0200

    Add watchdog file

---
+++ api/src/utils/watchdog.js
@@ -0,0 +1,29 @@
+import { getStatsDClient } from './statsd';
+
+const defaultSampleInterval = 60;
+
+export function startSampling(metricName, sampleInterval = defaultSampleInterval) {
+	var statsd = getStatsDClient();
+
+	function updateMetric(diff) {
+		const elapsed = diff[0] * 1000000 + diff[1] / 1000 - sampleInterval;
+		statsd.timing(metricName, elapsed);
+	}
+
+	function loop() {
+		let time = process.hrtime();
+
+		setTimeout(() => {
+			const diff = process.hrtime(time);
+
+			//XXX: scheduling statsd metric update to avoid disrupting measuring loop
+			process.nextTick(() => updateMetric(diff));
+
+			time = process.hrtime();
+
+			loop();
+		}, sampleInterval);
+	}
+
+	loop();
+}
