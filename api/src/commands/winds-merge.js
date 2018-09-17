c6cd16c70920a61ed7dfda7b13bc4e18841b6348
Max Klyga <max@getstream.io>
2018-09-17 13:29:16 +0200

    release v2.1.175

--- api/src/commands/winds-merge.js
+++ api/src/commands/winds-merge.js
@@ -2,14 +2,11 @@ import mongoose from 'mongoose';
 import ProgressBar from 'progress';
 
 import db from '../utils/db';
-import { mergeFeeds } from '../utils/merge';
-import { fetchSocialScore } from '../utils/social';
 import { upsertCollections } from '../utils/collections';
-import User from '../models/user';
+import RSS from '../models/rss';
+import Podcast from '../models/podcast';
 import Article from '../models/article';
-import Episode from '../models/article';
-
-const batchSize = 5000;
+import Episode from '../models/episode';
 
 function sleep(time) {
     return new Promise(resolve => setTimeout(resolve, time));
@@ -17,108 +14,137 @@ function sleep(time) {
 
 process.on('unhandledRejection', error => console.error(error.message));
 
+const feedModels = {
+    rss: { feed: RSS, content: Article },
+    podcast: { feed: Podcast, content: Episode }
+};
+
+function estimateSize(content) {
+    let size = 2; // {}
+    for (const [key, value] of Object.entries(content)) {
+        size += Buffer.byteLength(String(key), 'utf8');
+        size += Buffer.byteLength(String(value), 'utf8');
+        size += 2; // :,
+    }
+    return size;
+}
+
 async function main() {
     await db;
-    // await mergeFeeds('5aff4dd4fe7430d359cbb524', '5afdcfe4fe7430d359b7c234', 'podcast');
-    // await mergeFeeds('5aff4dd4fe7430d359cbb524', '5afdc6eefe7430d359b6e81d', 'podcast');
-    // return;
 
-    // const articles = mongoose.connection.collection('dup_articles_clean')
-    // const articleCount = await articles.count();
-    // const bar = new ProgressBar('[:current / :total] :bar [:percent | :rate records per second]', { total: articleCount });
-    // const cursor = articles.find().batchSize(batchSize);
+    for (const type of ['rss', 'podcast']) {
+        const model = feedModels[type];
+        console.log(`synchronising ${type} content`);
 
-    // let deletes = [];
-    // while (await cursor.hasNext()) {
-        // const data = await cursor.next();
-        // deletes.push(Article.deleteMany({ _id: { $in: data.ids } }));
-        // if (deletes.length % 128 === 0) {
-            // await Promise.all(deletes);
-            // deletes = [];
-        // }
-        // bar.tick();
-    // }
-    // await Promise.all(deletes);
-    const articles = mongoose.connection.collection('dup_episodes_clean')
-    const articleCount = await articles.count();
-    const bar = new ProgressBar('[:current / :total] :bar [:percent | :rate records per second]', { total: articleCount });
-    const cursor = articles.find().batchSize(batchSize);
+        const contentModelName = model.content.collection.collectionName;
+        const feedCount = await model.feed.countDocuments();
+        const bar = new ProgressBar('[:current / :total] :bar [:percent | :rate records per second]', { total: feedCount });
 
-    let deletes = [];
-    while (await cursor.hasNext()) {
-        const data = await cursor.next();
-        deletes.push(Episode.deleteMany({ _id: { $in: data.ids } }));
-        if (deletes.length % 128 === 0) {
-            await Promise.all(deletes);
-            deletes = [];
+        let lastFeedId = mongoose.Types.ObjectId('000000000000000000000000');
+        let feedsSynced = false;
+        while (!feedsSynced) {
+            try {
+                const feedCursor = model.feed.collection.find({
+                    _id: { $gte: lastFeedId }
+                }).sort({
+                    _id: 1
+                }).batchSize(32);
+                while (await feedCursor.hasNext()) {
+                    const feed = await feedCursor.next();
+                    const allowedLanguage = [null, undefined, '', 'eng'].includes(feed.language);
+                    if (!allowedLanguage) {
+                        bar.tick();
+                        continue;
+                    }
+
+                    let lastId = mongoose.Types.ObjectId('000000000000000000000000');
+                    let articlesSynced = false;
+                    while (!articlesSynced) {
+                        try {
+                            const cursor = model.content.collection.find({
+                                _id: { $gte: lastId },
+                                [type]: feed._id,
+                            }).sort({
+                                publicationDate: 1,
+                                _id: 1
+                            }).limit(1000).batchSize(1000);
+                            const articleCount = await model.content.countDocuments({ [type]: feed._id });
+                            let upserts = [];
+                            let content = [];
+                            let currentSize = 0;
+                            let mostRecentPublicationDate;
+                            const chunkSize = 1000;
+                            const sizeLimit = 100 * 1024; // less then 128Kb to leave some space for external data
+                            while (await cursor.hasNext()) {
+                                const source = await cursor.next();
+                                mostRecentPublicationDate = source.publicationDate;
+
+                                const item = {
+                                    id: source._id,
+                                    title: source.title,
+                                    likes: source.likes,
+                                    socialScore: source.socialScore,
+                                    description: source.description,
+                                    publicationDate: source.publicationDate,
+                                    [type]: source[type],
+                                };
+                                //XXX: we overestimate object size by 5-10%
+                                const size = estimateSize(item);
+                                const batchIsFull = content.length == chunkSize;
+                                const batchTooBig = currentSize + size > sizeLimit;
+                                if (batchIsFull || batchTooBig) {
+                                    upserts.push(upsertCollections(contentModelName, content));
+                                    content = [];
+                                    currentSize = 0;
+                                }
+                                if (upserts.length && upserts.length % 128 === 0) {
+                                    await Promise.all([
+                                        ...upserts,
+                                        // sleep(800)
+                                    ]);
+                                    upserts = [];
+                                }
+
+                                currentSize += size;
+                                content.push(item);
+                                lastId = item.id;
+                            }
+
+                            await Promise.all([
+                                ...upserts,
+                                upsertCollections(contentModelName, content),
+                                upsertCollections(type, [{
+                                    id: feed._id,
+                                    title: feed.title,
+                                    language: feed.language,
+                                    description: feed.description,
+                                    articleCount: content.length,
+                                    mostRecentPublicationDate
+                                }]),
+                                // sleep(800)
+                            ]);
+                            bar.tick();
+                            articlesSynced = true;
+                        } catch (err) {
+                            console.error(`\n\terror processing content: ${err.message}\nresuming from ${lastId}`);
+                        }
+                    }
+                    lastFeedId = feed._id;
+                }
+
+                console.log(`${type} synchronised\n`);
+                feedsSynced = true;
+            } catch (err) {
+                console.error(`\n\terror processing feed: ${err.message}\nresuming from ${lastFeedId}`);
+            }
         }
-        bar.tick();
     }
-    await Promise.all(deletes);
-
-    /*
-    const cursor = Article.collection.aggregate([
-        { $match: { fingerprint: 1 } },
-        { $lookup: { from: 'articles', localField: 'fingerprint', foreignField: 'fingerprint', as: 'duplicate' } },
-        { $unwind: '$duplicate' },
-        { $project: { rss: 1, dup_rss: "$duplicate.rss", original: { $eq: ["$_id", "$duplicate._id"] }, duplicate: { $ne: ["$_id", "$duplicate._id"] } } },
-        { $group: { _id: '$rss', dup_rss: { $addToSet: "$dup_rss" }, total: { $sum: 1 }, original: { $sum: { $cond: ['$original', 1, 0] } }, duplicates: { $sum: { $cond: ['$duplicate', 1, 0] } } } },
-        { $out: 'feed_duplicates' }
-    ], { allowDiskUse: true });
-    return await cursor.next();
-
-    console.log(`Finished reading ${articleCount} articles`);
-    console.log('Grouping by feed');
-
-    bar = new ProgressBar('[:current / :total] :bar [:percent | :rate records per second]', { total: articleCount });
-    const feedSummaries = new Map();
-    fingerprintToFeed.forEach(feedIDs => {
-        const singleFeed = feedIDs.length == 1;
-        const metric = singleFeed ? 'original' : 'duplicate';
-        for (const feedID of feedIDs) {
-            const summary = feedSummaries.get(feedID) || { total: 0, original: 0, duplicate: 0 };
-            summary.total += 1;
-            summary[metric] += 1;
-            feedSummaries.set(feedID, summary);
-        }
-        bar.tick();
-    });
-    console.log('Finished grouping by feed');
-    console.log('Sorting results');
-
-    const summaries = [...feedSummaries.entries()];
-    summaries.sort(([_, l], [__, r]) => {
-        if (l.total > r.total) {
-            return 1;
-        }
-        if (l.total < r.total) {
-            return -1;
-        }
-        if (l.duplicate > r.duplicate) {
-            return 1;
-        }
-        if (l.duplicate < r.duplicate) {
-            return -1;
-        }
-        if (l.original > r.original) {
-            return 1;
-        }
-        if (l.original < r.original) {
-            return -1;
-        }
-        return 0;
-    });
-    console.log('Finished sorting results');
-    for (const result of summaries) {
-        console.dir(result);
-    }
-    */
 }
 
 main().then(() => {
-	console.info('done');
+	console.log('\ndone');
 	process.exit(0);
 }).catch(err => {
-	console.info(`failed with err ${err.stack}`);
+	console.error(`\nfailed with err ${err.stack}`);
 	process.exit(1);
 });
