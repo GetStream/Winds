b0216fd977f5a26a1a45b6cbdcf1a9f0e311a11e
Amin Mahboubi <aminmahboobi@gmail.com>
2018-11-02 00:04:09 +0100

    feature/show hide featured #493

--- app/src/components/FeaturedItems.js
+++ app/src/components/FeaturedItems.js
@@ -6,6 +6,14 @@ import { Link } from 'react-router-dom';
 import { getFeatured } from '../api';
 
 class FeaturedItems extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			featured: localStorage.getItem('featured') || 'show',
+		};
+	}
+
 	componentDidMount() {
 		if (!this.props.featuredItems.length) getFeatured(this.props.dispatch);
 	}
@@ -15,33 +23,44 @@ class FeaturedItems extends React.Component {
 			<div className="featured-items-section">
 				<div className="featured-items-header">
 					<h2>Featured on Winds</h2>
+					<span
+						onClick={() => {
+							const featured =
+								this.state.featured === 'show' ? 'hide' : 'show';
+							this.setState({ featured });
+							localStorage.setItem('featured', featured);
+						}}
+					>
+						{this.state.featured === 'show' ? 'Hide' : 'Show'}
+					</span>
 				</div>
-				<div className="featured-item-list">
-					{this.props.featuredItems.map((featuredItem) => {
-						let linkURL = '#';
-						if (featuredItem.type === 'rss') {
-							linkURL = `/rss/${featuredItem._id}?featured=true`;
-						} else if (featuredItem.type === 'podcast') {
-							linkURL = `/podcasts/${featuredItem._id}?featured=true`;
-						}
-						return (
-							<Link
-								className="featured-item"
-								key={featuredItem._id}
-								style={{
-									backgroundImage: `linear-gradient(to top, black, transparent), url(${featuredItem
-										.images.featured ||
-										getPlaceholderImageURL(featuredItem._id)})`,
-								}}
-								to={linkURL}
-							>
-								<h1>{featuredItem.title}</h1>
-								<p />
-								<label>{featuredItem.type}</label>
-							</Link>
-						);
-					})}
-				</div>
+				{this.state.featured === 'show' && (
+					<div className="featured-item-list">
+						{this.props.featuredItems.map((featuredItem) => {
+							let linkURL = '#';
+							if (featuredItem.type === 'rss') {
+								linkURL = `/rss/${featuredItem._id}?featured=true`;
+							} else if (featuredItem.type === 'podcast') {
+								linkURL = `/podcasts/${featuredItem._id}?featured=true`;
+							}
+							return (
+								<Link
+									className="featured-item"
+									key={featuredItem._id}
+									style={{
+										backgroundImage: `linear-gradient(to top, black, transparent),
+										url(${featuredItem.images.featured || getPlaceholderImageURL(featuredItem._id)})`,
+									}}
+									to={linkURL}
+								>
+									<h1>{featuredItem.title}</h1>
+									<p />
+									<label>{featuredItem.type}</label>
+								</Link>
+							);
+						})}
+					</div>
+				)}
 			</div>
 		);
 	}
