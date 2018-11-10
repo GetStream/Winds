328f8924f3c600ac5d1379da2241287881ea6c37
Amin Mahboubi <aminmahboobi@gmail.com>
2018-11-10 14:45:45 +0100

    Moment Humanize local

--- app/src/components/TimeAgo/index.js
+++ app/src/components/TimeAgo/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import moment from 'moment';
+moment.updateLocale('en', {
+	relativeTime: {
+		past: '%s Ago',
+		s: 's',
+		ss: '%ds',
+		m: '%dm',
+		mm: '%dm',
+		h: '%dh',
+		hh: '%dh',
+		d: '%dd',
+		dd: '%dd',
+		M: 'a month',
+		MM: '%d month',
+		y: '%dy',
+		yy: '%dy',
+	},
+});
 
 const TimeAgo = (props) => {
 	let { timestamp, trim, ...rest } = props;
