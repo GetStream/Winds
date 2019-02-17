8de02df8224a9dec3247404e7e1043af2f17d8ea
Amin Mahboubi <aminmahboobi@gmail.com>
2019-02-17 14:39:43 +0100

    fix build script

--- api/build.sh
+++ api/build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+cd api
+
 # Remove the existing build directory and create a fresh one
-rm -rf api/dist && mkdir -p api/dist/{utils,email/templates}
+rm -rf dist && mkdir -p dist/{utils,email/templates}
 
 # Transpile ES6 to JavaScript
-npx babel api/src --out-dir api/dist --ignore node_modules
+npx babel src --out-dir dist --ignore node_modules
 
 # Copy build files to fresh /dist directory
-cp api/package.json api/dist/package.json
+cp package.json dist/package.json
 
 # Copy email files to fresh /dist directory
-cp -R api/src/utils/email/templates api/dist/utils/email
+cp -R src/utils/email/templates dist/utils/email
 
 # Install node modules via yarn
-cd api/dist && yarn install --production --modules-folder node_modules
+cd dist && yarn install --production --modules-folder node_modules
