39ab325e644292506fbbcf6a4f5d669743568b69
Amin Mahboubi <aminmahboobi@gmail.com>
2019-03-24 10:47:00 +0100

    express basic auth

---
+++ api/src/utils/basicAuth.js
@@ -0,0 +1,18 @@
+import basicAuth from 'express-basic-auth';
+import User from '../models/user';
+
+const asyncAuthorizer = async (email, password, cb) => {
+	if (!email || !password) return cb(null, false);
+
+	const user = await User.findOne({ email: email.toLowerCase().trim() });
+	if (!user) return cb(null, false);
+
+	if (!(await user.verifyPassword(password))) return cb(null, false);
+	return cb(null, true);
+};
+
+export default basicAuth({
+	authorizer: asyncAuthorizer,
+	authorizeAsync: true,
+	challenge: true,
+});
