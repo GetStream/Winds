b94c93aa7fec6d1e6b174cafae150c1bf69b4c48
Amin Mahboubi <aminmahboobi@gmail.com>
2019-03-24 11:02:02 +0100

    basicAuth is admin

--- api/src/utils/basicAuth.js
+++ api/src/utils/basicAuth.js
@@ -4,7 +4,7 @@ import User from '../models/user';
 const asyncAuthorizer = async (email, password, cb) => {
 	if (!email || !password) return cb(null, false);
 
-	const user = await User.findOne({ email: email.toLowerCase().trim() });
+	const user = await User.findOne({ email: email.toLowerCase().trim(), admin: true });
 	if (!user) return cb(null, false);
 
 	if (!(await user.verifyPassword(password))) return cb(null, false);
