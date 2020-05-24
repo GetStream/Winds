0d923be663641e1539a063cf5e7e1bef8cc1179d
Amin Mahboubi <aminmahboobi@gmail.com>
2020-05-24 17:02:45 +0200

    app/PWA

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Raven from 'raven-js';
 import stream from 'getstream';
 import StreamAnalytics from 'stream-analytics';
+import * as serviceWorker from './serviceWorker';
 
 import config from './config';
 
@@ -21,3 +22,8 @@ window.streamAnalyticsClient = new StreamAnalytics({
 Raven.context(() => {
 	ReactDOM.render(<App />, document.getElementById('root'));
 });
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
